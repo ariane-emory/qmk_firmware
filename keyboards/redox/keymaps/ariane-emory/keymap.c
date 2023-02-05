22efcbbdfd0e716f6278544651bb507c1b235d13
Ariane Emory <ariane.emory@protonmail.com>
2023-02-05 17:55:11 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -3,5 +3,10 @@
 const uint16_t PROGMEM dbl_spc_combo[] = {KC_SPC, RGUI_T(KC_BSPC), COMBO_END};
 const uint16_t PROGMEM dbl_osl_combo[] = {OSL(3), OSL(4), COMBO_END};
 
+combo_t key_combos[/* COMBO_COUNT */] = {
+  COMBO(dbl_spc_combo, KC_ESC)
+  COMBO(dbl_osl_combo, KC_TAB)
+};
+
 #include "keymap.inl"
 
