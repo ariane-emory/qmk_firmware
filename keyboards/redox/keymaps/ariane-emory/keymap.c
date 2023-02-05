2da3d9993c4403b0f96d5a62732a4edd5e16b760
Ariane Emory <ariane.emory@protonmail.com>
2023-02-05 18:51:40 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1,5 +1,9 @@
 #include QMK_KEYBOARD_H
 
+/* const uint16_t PROGMEM test_combo1[] = {KC_A, KC_S, COMBO_END}; */
 
+/* combo_t key_combos[COMBO_COUNT] = { */
+/*   COMBO(test_combo1, KC_ESC) */
+/* }; */
 
 #include "keymap.inl"
