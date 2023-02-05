c6b9165d0722212c5048a5167305d380bce440d5
Ariane Emory <ariane.emory@protonmail.com>
2023-02-05 16:17:28 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -4,8 +4,6 @@ const uint16_t PROGMEM double_space_combo [] = {KC_SPC, RGUI_T(KC_BSPC), COMBO_E
 const uint16_t PROGMEM double_tucky_combo [] = {OSL(3), OSL(4), COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-  COMBO(double_space_combo, LCTL(KC_SPC)),
-  COMBO(double_tucky_combo, LM(10, MOD_LGUI | MOD_LALT))
 };
 
 #include "keymap.inl"
