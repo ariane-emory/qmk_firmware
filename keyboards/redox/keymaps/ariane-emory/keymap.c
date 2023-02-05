c1ae9938d500961b16d2a87c74bedbb7be05cd66
Ariane Emory <ariane.emory@protonmail.com>
2023-02-05 16:26:57 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM double_tucky_combo [] = {OSL(3), OSL(4), COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   COMBO(double_space_combob, LCTL(KC_SPC)),
-  COMBO(double_tucky_combob, LCTL(KC_SPC))
+  COMBO(double_tucky_combob, LM(10, MOD_LGUI | MOD_LALT))
 };
 
 #include "keymap.inl"
