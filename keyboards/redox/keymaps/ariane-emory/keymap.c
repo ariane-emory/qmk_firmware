aeff1a001cec5380d5afd407584acf7562b51011
Ariane Emory <ariane.emory@protonmail.com>
2023-02-05 17:49:42 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1,4 +1,7 @@
 #include QMK_KEYBOARD_H
 
+const uint16_t PROGMEM dbl_spc_combo[] = {KC_SPC, RGUI_T(KC_BSPC), COMBO_END};
+const uint16_t PROGMEM dbl_osl_combo[] = {OSL(3), OSL(4), COMBO_END};
+
 #include "keymap.inl"
 
