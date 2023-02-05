03df4e4da9fd3282a30586f2f68fe28f99d61a1d
Ariane Emory <ariane.emory@protonmail.com>
2023-02-05 16:06:14 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1,3 +1,7 @@
 #include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM double_space_combo [] = {KC_SPC, RGUI_T(KC_BSPC), COMBO_END};
+const uint16_t PROGMEM double_tucky_combo [] = {OSL(3), OSL(4), COMBO_END};
+
 #include "keymap.inl"
 
