8a471dd11d28839b414aecaea11fbdba72203ed9
Ariane Emory <ariane.emory@protonmail.com>
2023-02-06 01:06:29 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1,5 +1,25 @@
 #include QMK_KEYBOARD_H
 
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, RGUI_T(KC_BSPC), KC_DEL);
+
+const key_override_t delete_key_override_2 = {
+  .trigger                = RGUI_T(KC_BSPC),
+  .trigger_mods           = MOD_MASK_SHIFT,
+  .layers                 = (1 << 0),
+  .suppressed_mods        = MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT),
+  .options                = ko_option_no_reregister_trigger,
+  .negative_mod_mask      = 0,
+  .custom_action          = NULL,
+  .context                = NULL,
+  .replacement            = KC_DEL,
+  .enabled                = NULL};
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+  &delete_key_override_2,
+  NULL // Null terminate the array of overrides!
+};
+
 enum td_events {
   TD_SPC_LALT,
   TD_BSPC_RGUI
