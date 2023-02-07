e1c5170d82d44378dc4f22ddde3d1466624f6eaa
Ariane Emory <ariane.emory@protonmail.com>
2023-02-07 15:17:07 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -42,3 +42,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_SHIFT_TIMEOUT 250
 #define RETRO_SHIFT
 #define ONESHOT_TIMEOUT 1000
+
+#define MOUSEKEY_DELAY 0 // 10
+// ^ Delay between pressing a movement key and cursor movement.
+#define MOUSEKEY_INTERVAL 20
+// ^ Time between cursor movements in milliseconds.
+#define MOUSEKEY_MOVE_DELTA 8
+// ^ Step size.
+#define MOUSEKEY_MAX_SPEED 10
+// ^ Maximum cursor speed at which acceleration stops.
+#define MOUSEKEY_TIME_TO_MAX 30
+// ^ Time until maximum cursor speed is reached.
+#define MOUSEKEY_WHEEL_DELAY 0 // 10
+// ^ Delay between pressing a wheel key and wheel movement.
+#define MOUSEKEY_WHEEL_INTERVAL 80
+// ^ Time between wheel movements.
+#define MOUSEKEY_WHEEL_MAX_SPEED 8
+// ^ Maximum number of scroll steps per scroll action.
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+// ^ yTime until maximum scroll speed is reached.
+
+//#define MOUSEKEY_INERTIA 1
