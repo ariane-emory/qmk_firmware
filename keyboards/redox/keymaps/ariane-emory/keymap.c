2c493b51289c1acd11158a9a5544990b8ee6772d
Ariane Emory <ariane.emory@protonmail.com>
2023-02-10 19:52:34 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -68,7 +68,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case SS_UPDIR:
     if (record->event.pressed) {
-      SEND_STRING(" ../");
+      SEND_STRING("../");
     }
     return false;
   }
