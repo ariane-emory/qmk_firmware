3e4fd3ef4018ea278ab01e3b8cf47859516601f2
Ariane Emory <ariane.emory@protonmail.com>
2023-04-15 22:57:57 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -16,17 +16,31 @@
 // Combos
 // ================================================================================
 
-// enum combo_events {
+enum combo_events {
 //   DBL_SPC,
-//   COMBO_LENGTH
+  DBL_TUCK,
+  COMBO_LENGTH
+ };
+
+uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
+
+// const uint16_t PROGMEM dbl_spc_combo[] = {
+//  LALT_T(KC_SPC),
+//  RGUI_T(KC_BSPC),
+//  COMBO_END
 // };
-// uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
 
-// const uint16_t PROGMEM dbl_spc_combo[] = {LALT_T(KC_SPC), RGUI_T(KC_BSPC), COMBO_END};
+const uint16_t PROGMEM dbl_tuck_combo[] = {
+  OSL(3),
+  OSL(4),
+  COMBO_END
+};
 
-// combo_t key_combos[] = {
+
+combo_t key_combos[] = {
 //  [DBL_SPC]       = COMBO(dbl_spc_combo, RCTL(KC_SPC)),
-// };
+  [DBL_TUCK]      = COMBO(dbl_tuck_combo, KC_LCTL),
+};
 
 // ================================================================================
 // Mod tap interrupt
