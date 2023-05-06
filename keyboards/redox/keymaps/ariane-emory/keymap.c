0d374dfd1fed2bd0fd130494b6dc95bd9665747c
Ariane Emory <ariane.emory@protonmail.com>
2023-05-06 18:12:25 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -64,11 +64,17 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 // ================================================================================
 
 enum arianes_keycodes {
-  SS_UPDIR = SAFE_RANGE
+  SS_UPDIR = SAFE_RANGE,
+  SS_NUM
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+  case SS_NUM:
+    if (record->event.pressed) {
+      SEND_STRING("6851");
+    }
+    return false;
   case SS_UPDIR:
     if (record->event.pressed) {
       SEND_STRING("../");
