245ef5bd273d7f10d945225a24176a0e40247550
Ariane Emory <ariane.emory@protonmail.com>
2023-05-07 05:02:46 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -16,35 +16,35 @@
 // Combos
 // ================================================================================
 
-#define CC(name, ...)                                                           \
-  const uint16_t PROGMEM name[] = { __VA_ARGS__, COMBO_END};
+#define DEFINE_COMBO_KEYS(name, ...)                                                           \
+  const uint16_t PROGMEM keys_ ## name[] = { __VA_ARGS__, COMBO_END};
 
-CC(keys_dbl_tuck_combo, MO(3), MO(4));
-CC(keys_ik,             KC_I, KC_K);
-CC(keys_mo3f,           MO(3), KC_F);
-CC(keys_mo4j,           MO(4), KC_J);
+DEFINE_COMBO_KEYS(dbl_tuck_combo, MO(3), MO(4));
+DEFINE_COMBO_KEYS(ik,             KC_I, KC_K);
+DEFINE_COMBO_KEYS(mo3f,           MO(3), KC_F);
+DEFINE_COMBO_KEYS(mo4j,           MO(4), KC_J);
 
-CC(keys_yu,             KC_Y, KC_U);
-CC(keys_ui,             KC_U, KC_I);
-CC(keys_io,             KC_I, KC_O);
-CC(keys_op,             KC_O, KC_P);
-CC(keys_uo,             KC_U, KC_O);
+DEFINE_COMBO_KEYS(yu,             KC_Y, KC_U);
+DEFINE_COMBO_KEYS(ui,             KC_U, KC_I);
+DEFINE_COMBO_KEYS(io,             KC_I, KC_O);
+DEFINE_COMBO_KEYS(op,             KC_O, KC_P);
+DEFINE_COMBO_KEYS(uo,             KC_U, KC_O);
 
-CC(keys_hj,             KC_H, KC_J);
+DEFINE_COMBO_KEYS(hj,             KC_H, KC_J);
 
-CC(keys_jk,             KC_J, KC_K);
+DEFINE_COMBO_KEYS(jk,             KC_J, KC_K);
 
-CC(keys_kl,             KC_K, KC_L);
-CC(keys_lquot,          KC_L, KC_QUOT);
-CC(keys_jl,             KC_J, KC_L);
+DEFINE_COMBO_KEYS(kl,             KC_K, KC_L);
+DEFINE_COMBO_KEYS(lquot,          KC_L, KC_QUOT);
+DEFINE_COMBO_KEYS(jl,             KC_J, KC_L);
 
-CC(keys_nm,             KC_N, KC_M);
-CC(keys_mcomma,         KC_M, KC_COMM);
-CC(keys_commadot,       KC_COMM, KC_DOT);
-CC(keys_dotslash,       KC_DOT, KC_SLASH);
-CC(keys_mdot,           KC_M, KC_DOT);
+DEFINE_COMBO_KEYS(nm,             KC_N, KC_M);
+DEFINE_COMBO_KEYS(mcomma,         KC_M, KC_COMM);
+DEFINE_COMBO_KEYS(commadot,       KC_COMM, KC_DOT);
+DEFINE_COMBO_KEYS(dotslash,       KC_DOT, KC_SLASH);
+DEFINE_COMBO_KEYS(mdot,           KC_M, KC_DOT);
 
-#undef CC
+#undef DEFINE_COMBO_KEYS
 
 combo_t key_combos[] = {
   COMBO(keys_dbl_tuck_combo, KC_LCTL),
@@ -53,13 +53,9 @@ combo_t key_combos[] = {
   COMBO(keys_mo3f,     MO(5)),
   COMBO(keys_mo4j,     MO(9)),
 
-  /* COMBO(keys_yu,       KC_EQL), */
   COMBO(keys_ui,       RALT(KC_B)), 
   COMBO(keys_io,       RALT(KC_F)), 
-  /* COMBO(keys_op,       KC_EQL), */
-  /* COMBO(keys_uo,       KC_EQL), */
 
-  /* COMBO(keys_hj,       KC_EQL), */
   COMBO(keys_jk,       KC_LBRC),
   COMBO(keys_kl,       KC_RBRC),
   COMBO(keys_lquot,    KC_SCLN), 
@@ -67,9 +63,6 @@ combo_t key_combos[] = {
 
   COMBO(keys_nm,       LGUI(KC_Z)),
   COMBO(keys_mcomma,   KC_ENT),
-  /* [EVENT_COMMADOT] = COMBO(keys_commadot, KC_NO), */
-  /* [EVENT_DOTSLASH] = COMBO(keys_dotslash, KC_NO), */
-  /* [EVENT_MDOT]     = COMBO(keys_mdot,     KC_NO), */
 };
 
 uint16_t COMBO_LEN = ARRAY_SIZE(key_combos);
