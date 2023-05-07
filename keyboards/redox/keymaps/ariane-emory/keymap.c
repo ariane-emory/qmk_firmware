c41dfa43639477a9fd661a2675925aaa9a81c1d2
Ariane Emory <ariane.emory@protonmail.com>
2023-05-07 03:20:20 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -18,13 +18,19 @@
 
 enum combo_events {
   EVENT_DBL_TUCK,
-  /* EVENT_UI, */
-  /* EVENT_IO, */
-  /* EVENT_UO, */
+
+  EVENT_YU,
+  EVENT_UI,
+  EVENT_IO,
+  EVENT_UO,
+  EVENT_OP,
+
   EVENT_HJ,
   EVENT_JK,
   EVENT_KL,
   EVENT_JL,
+  EVENT_LQUOT,
+
   EVENT_MCOMMA,
   COMBO_LENGTH
 };
@@ -32,24 +38,36 @@ enum combo_events {
 uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
 
 const uint16_t PROGMEM keys_dbl_tuck_combo[] = { MO(3), MO(4), COMBO_END };
-/* const uint16_t PROGMEM keys_ui[]        = { KC_U, KC_I, COMBO_END }; */
-/* const uint16_t PROGMEM keys_io[]        = { KC_I, KC_O, COMBO_END }; */
-/* const uint16_t PROGMEM keys_uo[]        = { KC_U, KC_O, COMBO_END }; */
+
+const uint16_t PROGMEM keys_yu[]        = { KC_Y, KC_U, COMBO_END };
+const uint16_t PROGMEM keys_ui[]        = { KC_U, KC_I, COMBO_END };
+const uint16_t PROGMEM keys_io[]        = { KC_I, KC_O, COMBO_END };
+const uint16_t PROGMEM keys_uo[]        = { KC_U, KC_O, COMBO_END };
+const uint16_t PROGMEM keys_op[]        = { KC_O, KC_P, COMBO_END };
+
 const uint16_t PROGMEM keys_hj[]        = { KC_H, KC_J, COMBO_END };
 const uint16_t PROGMEM keys_jk[]        = { KC_J, KC_K, COMBO_END };
 const uint16_t PROGMEM keys_kl[]        = { KC_K, KC_L, COMBO_END };
 const uint16_t PROGMEM keys_jl[]        = { KC_J, KC_L, COMBO_END };
+const uint16_t PROGMEM keys_lquot[]     = { KC_L, KC_QUOT, COMBO_END };
+
 const uint16_t PROGMEM keys_mcomma[]    = { KC_M, KC_COMM, COMBO_END };
 
 combo_t key_combos[] = {
   [EVENT_DBL_TUCK] = COMBO(keys_dbl_tuck_combo, KC_LCTL),
-  /* [EVENT_UI]       = COMBO(keys_ui, KC_LCBR), */
-  /* [EVENT_IO]       = COMBO(keys_io, KC_RCBR), */
-  /* [EVENT_IO]       = COMBO(keys_uo, KC_RCBR), */
+
+  [EVENT_YU]       = COMBO(keys_yu, KC_EQL),  
+  [EVENT_UI]       = COMBO(keys_ui, KC_LBRC), 
+  [EVENT_IO]       = COMBO(keys_io, KC_RBRC), 
+  [EVENT_UO]       = COMBO(keys_uo, KC_EQL),  
+  [EVENT_OP]       = COMBO(keys_uo, KC_EQL),  
+
   [EVENT_HJ]       = COMBO(keys_hj, KC_EQL),
   [EVENT_JK]       = COMBO(keys_jk, KC_LBRC),
   [EVENT_KL]       = COMBO(keys_kl, KC_RBRC),
   [EVENT_JL]       = COMBO(keys_jl, KC_EQL),
+  [EVENT_LQUOT]    = COMBO(keys_jl, KC_EQL),  
+
   [EVENT_MCOMMA]   = COMBO(keys_mcomma, KC_ENT)
   };
 
