280a7f83a824aa0113a40e81fb3648d4f4363903
Ariane Emory <ariane.emory@protonmail.com>
2023-05-07 22:00:55 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -105,10 +105,10 @@ bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
    switch (keycode) {
    case QK_MOD_TAP ... QK_MOD_TAP_MAX:
-     /* if (keycode == MT(MOD_LALT,KC_SPC)) { */
-     /*   return false; */
-     /* } */
-     /* else */ {
+     if (keycode == MT(MOD_LALT,KC_SPC)) {
+       return false;
+     }
+     else {
        return true;
      }
   default:
@@ -150,8 +150,8 @@ uint16_t get_autoshift_timeout(uint16_t keycode, keyrecord_t *record) {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-      /* case SFT_T(KC_SPC): */
-      /*   return 5000; */
+    case LALT_T(KC_SPC):
+      return 5000;
     default:
       return TAPPING_TERM;
     }
