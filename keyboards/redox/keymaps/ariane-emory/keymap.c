69169ae7d93cc114ad24003ce4b5a2181affaf2c
Ariane Emory <ariane.emory@protonmail.com>
2023-05-09 16:00:12 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -40,7 +40,9 @@ enum arianes_keycodes {
     }                                                                           \
     return false;
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#define KEYRECORD_FUN(name) name(uint16_t keycode, keyrecord_t *record)
+
+bool KEYRECORD_FUN(process_record_user) {
   switch (keycode) {
     SEND_STRING_WITHOUT_MODS_CASE(SS_PIN1,       AE_PIN1);
     SEND_STRING_WITHOUT_MODS_CASE(SS_PIN2,       AE_PIN2);
@@ -55,8 +57,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 }
 
-#undef SEND_STRING_WITHOUT_MODS
-  
 // ================================================================================
 // Include combos 
 // ================================================================================
@@ -67,7 +67,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // Mod tap interrupt
 // ================================================================================
 
-bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+bool KEYRECORD_FUN(get_hold_on_other_key_press) {
   switch (keycode) {
   case QK_MOD_TAP ... QK_MOD_TAP_MAX:
     if (keycode == LCTL_T(KC_ESC)  ||
@@ -84,7 +84,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
   }
 }
 
-bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+bool KEYRECORD_FUN(get_permissive_hold) {
   switch (keycode) {
   case MT(MOD_LALT,KC_SPC):
     // Do not select the hold action when another key is tapped.
@@ -99,7 +99,7 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 // Autoshift
 // ================================================================================
 
-uint16_t get_autoshift_timeout(uint16_t keycode, keyrecord_t *record) {
+uint16_t KEYRECORD_FUN(get_autoshift_timeout) {
   switch(keycode) {
   case AUTO_SHIFT_ALPHA:
     return get_generic_autoshift_timeout() + 60;
@@ -115,7 +115,7 @@ uint16_t get_autoshift_timeout(uint16_t keycode, keyrecord_t *record) {
 // Tapping term
 // ================================================================================
 
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+uint16_t KEYRECORD_FUN(get_tapping_term) {
   switch (keycode) {
   case MT(MOD_LALT,KC_SPC):
     return TAPPING_TERM + 60;
@@ -124,6 +124,13 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   }
 }
 
+// ================================================================================
+// Undefine local macros
+// ================================================================================
+
+#undef KEYRECORD_FUN
+#undef SEND_STRING_WITHOUT_MODS
+  
 // ================================================================================
 // Include keymap
 // ================================================================================
