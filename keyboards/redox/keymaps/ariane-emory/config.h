d18d2d3048fd16b0627cdc29aa4723e5e6a828be
Ariane Emory <ariane.emory@protonmail.com>
2023-05-12 13:59:49 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* #define RGBLIGHT_SAT_STEP 8 */
 /* #define RGBLIGHT_VAL_STEP 8 */
 
-#define AUTO_SHIFT_MODIFIERS
+// #define AUTO_SHIFT_MODIFIERS
 #define AUTO_SHIFT_REPEAT
 #define AUTO_SHIFT_TIMEOUT 150
 #define AUTO_SHIFT_TIMEOUT_PER_KEY 
