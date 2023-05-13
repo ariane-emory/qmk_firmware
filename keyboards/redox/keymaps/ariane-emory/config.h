d586e509b3fcabdddd2de37719946b9d825ef463
Ariane Emory <ariane.emory@protonmail.com>
2023-05-13 12:57:52 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -70,7 +70,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM_PER_KEY
 
 #define TOGGLED_LAYER 6
-#define TOGGLED_LAYER_TIMEOUT 5000
+#define TOGGLED_LAYER_TIMEOUT 15000
 
 #define TRI_LAYER_ADJUST_LAYER 13
 #define TRI_LAYER_LOWER_LAYER 7
