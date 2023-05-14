a6718237aa30aacd35d49faeb8505818947bfd55
Ariane Emory <ariane.emory@protonmail.com>
2023-05-13 23:15:06 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -83,7 +83,10 @@ enum arianes_keycodes {
 
 static uint16_t idle_timer = 0;
 static bool asleep = false;
+
+#ifdef IGNORE_WAKING_KEY
 static uint16_t waking_key = KC_NO;
+#endif
 
 KEYRECORD_FUN(process_record_user, bool) {
   idle_timer = timer_read();
@@ -100,6 +103,9 @@ KEYRECORD_FUN(process_record_user, bool) {
     
     return false;
   }
+#else
+  if (asleep)
+    asleep = false;
 #endif
   
   if (!process_achordion(keycode, record))
