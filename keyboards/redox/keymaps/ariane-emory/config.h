bc3fbc7416328e32e6863921cb82784fca1832cf
Ariane Emory <ariane.emory@protonmail.com>
2023-05-16 15:41:55 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -75,7 +75,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_TIMEOUT                45000
 #define HSV_ASLEEP                 HSV_BLACK
-#define HSV_TOGGLED_LAYER_OFF      HSV_RED
+#define HSV_TOGGLED_LAYER_OFF      HSV_BLACK // HSV_RED
 #define HSV_TOGGLED_LAYER_ON       HSV_CHARTREUSE
 #define HSV_LOWER_LAYER_ON         HSV_PURPLE
 #define HSV_UPPER_LAYER_ON         HSV_BLUE
