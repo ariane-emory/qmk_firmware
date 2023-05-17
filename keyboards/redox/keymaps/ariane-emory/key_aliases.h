3e1a3f597551111c6839c9a3fe576055d72971c6
Ariane Emory <ariane.emory@protonmail.com>
2023-05-16 20:03:13 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/key_aliases.h
+++ keyboards/redox/keymaps/ariane-emory/key_aliases.h
@@ -1,1 +1,19 @@
-#pragma once 
+#pragma once
+
+#define QHA    LSFT_T(KC_A)
+#define QHS    LGUI_T(KC_S)
+#define QHD    LALT_T(KC_D)
+#define QHF    LCTL_T(KC_F)
+#define QHJ    RCTL_T(KC_J)
+#define QHK    RALT_T(KC_K)
+#define QHL    RGUI_T(KC_L)
+#define QHQUOT RSFT_T(KC_QUOT)
+
+#define QBZ    KC_Z
+#define QBX    KC_X
+#define QBC    KC_C
+#define QBV    KC_V
+#define QBM    KC_M
+#define QBCOMM KC_COMM
+#define QBDOT  KC_DOT
+#define QBSLSH KC_SLSH
