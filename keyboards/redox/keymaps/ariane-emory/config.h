a549a8a154c5155b38c3f199efd3e0e3ae9727f5
Ariane Emory <ariane.emory@protonmail.com>
2023-05-17 02:24:23 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -34,7 +34,7 @@
 
 #define MOUSEKEY_DELAY             0
 #define MOUSEKEY_INTERVAL          20 // 20
-#define MOUSEKEY_MAX_SPEED         8  // 9
+#define MOUSEKEY_MAX_SPEED         7  // 9
 #define MOUSEKEY_MOVE_DELTA        8
 #define MOUSEKEY_TIME_TO_MAX       40 // 38
 #define MOUSEKEY_WHEEL_DELAY       0
