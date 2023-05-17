40bdc1657e2115b53ab25fe09495133f3eef9f44
Ariane Emory <ariane.emory@protonmail.com>
2023-05-16 20:55:09 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/key_aliases.h
+++ keyboards/redox/keymaps/ariane-emory/key_aliases.h
@@ -4,6 +4,8 @@
 #define QH_S    LGUI_T(KC_S)
 #define QH_D    LALT_T(KC_D)
 #define QH_F    LCTL_T(KC_F)
+#define QH_G    KC_G
+#define QH_H    KC_H
 #define QH_J    RCTL_T(KC_J)
 #define QH_K    RALT_T(KC_K)
 #define QH_L    RGUI_T(KC_L)
@@ -13,6 +15,8 @@
 #define QB_X    LALT_T(KC_X)
 #define QB_C    LGUI_T(KC_C)
 #define QB_V    LSFT_T(KC_V)
+#define QB_B    KC_B
+#define QB_N    KC_N
 #define QB_M    RSFT_T(KC_M)
 #define QB_COMM RGUI_T(KC_COMM)
 #define QB_DOT  RALT_T(KC_DOT)
@@ -22,6 +26,8 @@
 #define CH_R    LGUI_T(KC_R)
 #define CH_S    LALT_T(KC_S)
 #define CH_T    LCTL_T(KC_T)
+#define CH_D    KC_D
+#define CH_H    KC_H
 #define CH_N    RCTL_T(KC_N)
 #define CH_E    RALT_T(KC_E)
 #define CH_I    RGUI_T(KC_I)
@@ -31,6 +37,8 @@
 #define CB_X    LALT_T(KC_X)
 #define CB_C    LGUI_T(KC_C)
 #define CB_V    LSFT_T(KC_V)
+#define CB_B    KC_B
+#define CB_K    KC_K
 #define CB_M    RSFT_T(KC_M)
 #define CB_COMM RGUI_T(KC_COMM)
 #define CB_DOT  RALT_T(KC_DOT)
