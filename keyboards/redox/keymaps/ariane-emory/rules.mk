3a34dfbb984b1499453914b9567918a588e19209
Ariane Emory <ariane.emory@protonmail.com>
2023-05-19 16:45:27 -0400

    may have broken rgb

--- keyboards/redox/keymaps/ariane-emory/rules.mk
+++ keyboards/redox/keymaps/ariane-emory/rules.mk
@@ -1,20 +1,22 @@
-COMMAND_ENABLE     = no
-CONSOLE_ENABLE     = no
-GRAVE_ESC_ENABLE   = no 
-MAGIC_ENABLE       = no
-SPACE_CADET_ENABLE = no
-TAP_DANCE_ENABLE   = no
+COMMAND_ENABLE     	 = no
+CONSOLE_ENABLE     	 = no
+GRAVE_ESC_ENABLE   	 = no 
+MAGIC_ENABLE       	 = no
+SPACE_CADET_ENABLE 	 = no
+TAP_DANCE_ENABLE   	 = no
 
-LTO_ENABLE         = yes
+LTO_ENABLE         	 = yes
 
-AUTO_SHIFT_ENABLE  = yes
-CAPS_WORD_ENABLE   = yes
-COMBO_ENABLE       = yes
-NKRO_ENABLE        = yes
-RGBLIGHT_ENABLE    = yes 
-SEND_STRING_ENABLE = yes
-TRI_LAYER_ENABLE   = yes
+AUTO_SHIFT_ENABLE  	 = yes
+CAPS_WORD_ENABLE   	 = yes
+COMBO_ENABLE       	 = yes
+NKRO_ENABLE        	 = yes
+RGBLIGHT_ENABLE    	 = yes 
+SEND_STRING_ENABLE 	 = yes
+TRI_LAYER_ENABLE   	 = yes
+DEFERRED_EXEC_ENABLE = yes
 
 SRC += features/achordion.c
+
 # OPT = 1
 # OPT_DEFS += -Os
