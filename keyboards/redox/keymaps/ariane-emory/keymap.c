731b64b23400609324ae6eac204330fb28df1932
Ariane Emory <ariane.emory@protonmail.com>
2023-05-27 03:52:22 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1,9 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #include "lwsm/src/lwsm.h"
-#include "lwsm/src/rgb.h"
 #include "lwsm/src/rgb_fader.h"
-#include "lwsm/src/rgb_fader_lwsm.h"
 
 #if defined(HOME_ROW_MODS) || defined(BOTTOM_ROW_MODS)
 #define USE_ACHORDION
