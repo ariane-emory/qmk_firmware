5437d0f8f81fd4231cd2983e85f7fa23c651f623
Ariane Emory <ariane.emory@protonmail.com>
2023-05-31 01:59:44 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -284,10 +284,19 @@ bool achordion_chord(
   keyrecord_t * other_record) {
   if (
     (IS_LAYER_ON(0) && (
-#ifdef HOME_ROW_MODS
-#  ifndef STAGGERED_SHIFT
+#ifndef HOME_SHIFT
       tap_hold_keycode == QH_A    ||
-#  endif
+      tap_hold_keycode == QH_QUOT ||
+      tap_hold_keycode == CH_A    ||
+      tap_hold_keycode == CH_QUOT ||
+#endif
+#if defined(BOTTOM_SHIFT) && ! defined(BOTTOM_ROW_MODS)
+      tap_hold_keycode == QB_Z    ||
+      tap_hold_keycode == QB_SLSH ||
+      tap_hold_keycode == CB_Z    ||
+      tap_hold_keycode == CB_SLSH ||
+#endif
+#ifdef HOME_ROW_MODS
       tap_hold_keycode == QH_S    ||
       tap_hold_keycode == QH_D    ||
       tap_hold_keycode == QH_F    ||
@@ -296,10 +305,7 @@ bool achordion_chord(
       tap_hold_keycode == QH_J    ||
       tap_hold_keycode == QH_K    ||
       tap_hold_keycode == QH_L    ||
-#  ifndef STAGGERED_SHIFT
-      tap_hold_keycode == QH_QUOT ||
-#  endif
-#endif
+#endif // HOME_ROW_MODS
 #ifdef BOTTOM_ROW_MODS
       tap_hold_keycode == QB_Z    ||
       tap_hold_keycode == QB_X    ||
