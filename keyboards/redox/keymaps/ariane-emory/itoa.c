c4cab99d7091d8728295cb3c521e6afe4e6c1ecb
Ariane Emory <ariane.emory@protonmail.com>
2023-06-05 04:46:30 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/itoa.c
+++ keyboards/redox/keymaps/ariane-emory/itoa.c
@@ -25,16 +25,45 @@ void print_num(uint8_t num, const uint8_t max_digits) {
   
   printf("ix                   = %d\n", ix);
   printf("str    @ %p = '%s'\n", &buf[ix+1], &buf[ix]);
-  printf("cs     @ %p = '", &buf[ix+1]);
+  
+  for (uint8_t i = 0; i < size; i++)
+    printf("buf[%d] @ %p = %d\n", i, &buf[i], buf[i]);
+
+  printf("\n");
+}
+
+void print_num2(uint8_t num, const uint8_t max_digits) {
+  const uint8_t size = max_digits + 1;
+  char buf[size];
+  uint8_t ix = size - 2;
+  for (uint8_t i = 0; i < size; i++) buf[i] = 0;
+
+  if (num == 0) {
+    buf[size - 2] = '0';
+    ix     = size - 2;
+  } else {  
+    while (num) {
+      const uint8_t modulo = num % 10;
+      printf("modulo '%d'\n", modulo);
+    
+      num -= modulo;
+      num /= 10;
+      buf[ix--] = 48 + modulo;
+    }
+
+    ix++;
+  }
+  
+  printf("ix                   = %d\n", ix);
+  printf("str     @ %p = '", &buf[ix+1]);
   while (ix < max_digits) {
     printf("%c", buf[ix]);
     ix++;
   }
   printf("'\n");
-  
-  
+
   for (uint8_t i = 0; i < size; i++)
-           printf("buf[%d] @ %p = %d\n", i, &buf[i], buf[i]);
+    printf("buf[%d] @ %p = %d\n", i, &buf[i], buf[i]);
 
   printf("\n");
 }
