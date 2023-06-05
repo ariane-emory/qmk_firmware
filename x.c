d9ebbd9cfee41eab70da90535ea7003471cd9a8c
Ariane Emory <ariane.emory@protonmail.com>
2023-06-05 04:00:23 -0400

    ...

--- x.c
+++ x.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main() {
+void print_num(uint8_t num, const uint8_t size) {
   const uint8_t siz = 5;
   char buf[siz] = { 0 };
   uint8_t ix = siz - 2;
     
-  uint8_t num = 123;
-
   while (num) {
     const uint8_t modulo = num % 10;
     printf("modulo '%d'\n", modulo);
@@ -23,7 +21,15 @@ int main() {
     
   printf("\nstr  @ %p   = '%s'\n", &buf[ix+1], &buf[ix+1]);
   printf("ix                   = %d\n", ix);
-
+  printf("\n");
+  
   for (uint8_t i = 0; i < siz; i++)
     printf("buf[%d] @ %p = %d\n", i, &buf[i], buf[i]);
+
+  printf("\n");
+}
+
+int main() {
+  print_num(123, 5);
+  print_num(123, 4);
 }
