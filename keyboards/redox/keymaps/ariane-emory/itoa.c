6691913d693cf5afda39e7171493c593f1d1f3bd
Ariane Emory <ariane.emory@protonmail.com>
2023-06-05 04:19:12 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/itoa.c
+++ keyboards/redox/keymaps/ariane-emory/itoa.c
@@ -5,27 +5,22 @@ void print_num(uint8_t num, const uint8_t max_digits) {
   const uint8_t size = max_digits + 1;
   char buf[size];
   uint8_t ix = size - 2;
+  for (uint8_t i = 0; i < size; i++) buf[i] = 0;
 
   if (num == 0) {
     buf[0] = '0';
-    buf[1] = '\0';
     ix     = 0;
   } else {  
-    for (uint8_t i = 0; i < size; i++)
-      buf[i] = 0;
-
     while (num) {
-      ix--;
-      
       const uint8_t modulo = num % 10;
       printf("modulo '%d'\n", modulo);
     
       num -= modulo;
       num /= 10;
       if (modulo == 0) {
-        buf[ix] = 48 + modulo;
+        buf[ix--] = 48 + modulo;
       } else {
-        buf[ix] = 48 + modulo;
+        buf[ix--] = 48 + modulo;
       }
     }
 
