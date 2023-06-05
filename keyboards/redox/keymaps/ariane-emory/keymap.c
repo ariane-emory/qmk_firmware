cae84e4041c3da026bab0328923efda620c59c7c
Ariane Emory <ariane.emory@protonmail.com>
2023-06-05 14:49:27 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -84,18 +84,18 @@ static uint16_t idle_timer = 0;
 #define CR SS_TAP(X_ENT)
 
 #define FOR_EACH_CTRLABLE_OR_ALTABLE_SEND_STRING_KEYCODE(DO)                                                            \
-  DO(SS_SIRI,           (SS_DOWN(X_F24)SS_DELAY(50)SS_TAP(X_SPC)SS_UP(X_F24)), (""),                   (""))            \
-  DO(SS_DICT,           (SS_TAP(X_F24)SS_TAP(X_F24)),                          (""),                   (""))            \
-  DO(EM_LASTARG,        (" "SS_LCTL("c")SS_DELAY(50)"."),                      (""),                   (""))            \
-  DO(EM_CHG_BUF,        (SS_LCTL("x")SS_DELAY(50)"b"),                         (""),                   (""))            \
-  DO(EM_REVERT,         (SS_LCTL("x")SS_DELAY(50)SS_LCTL("r")),                (""),                   (""))            \
-  DO(SS_BANGBANG,       ("11"SS_TAP(X_ENT)),                                   (""),                   (""))            \
-  DO(SS_PIN1,           (AE_PIN1),                                             (AE_PIN2),              (""))            \
-  DO(SS_ARROW,          ("->"),                                                ("490"LL),              ("``"LL))        \
-  DO(SS_DIR,            ("~/"),                                                ("../"),                ("./"))          \
-  DO(SS_LBRACK,         ("9"),                                                 ("{"),                  ("["))           \
-  DO(SS_RBRACK,         ("0"),                                                 ("}"),                  ("]"))           \
-  DO(SS_BRACKS,         ("90" LL),                                             ("{" CR CR "}" UU TB),  ("[]" LL))
+  DO(SS_SIRI,           (SS_DOWN(X_F24)SS_DELAY(50)SS_TAP(X_SPC)SS_UP(X_F24)),  (""),                   (""))           \
+  DO(SS_DICT,           (SS_TAP(X_F24)SS_TAP(X_F24)),                           (""),                   (""))           \
+  DO(EM_LASTARG,        (" "SS_LCTL("c")SS_DELAY(50)"."),                       (""),                   (""))           \
+  DO(EM_CHG_BUF,        (SS_LCTL("x")SS_DELAY(50)"b"),                          (""),                   (""))           \
+  DO(EM_REVERT,         (SS_LCTL("x")SS_DELAY(50)SS_LCTL("r")),                 (""),                   (""))           \
+  DO(SS_BANGBANG,       ("11"SS_TAP(X_ENT)),                                    (""),                   (""))           \
+  DO(SS_PIN1,           (AE_PIN1),                                              (AE_PIN2),              (""))           \
+  DO(SS_ARROW,          ("->"),                                                 ("490"LL),              ("``"LL))       \
+  DO(SS_DIR,            ("~/"),                                                 ("../"),                ("./"))         \
+  DO(SS_LBRACK,         ("9"),                                                  ("{"),                  ("["))          \
+  DO(SS_RBRACK,         ("0"),                                                  ("}"),                  ("]"))          \
+  DO(SS_BRACKS,         ("90" LL),                                              ("{" CR CR "}" UU TB),  ("[]" LL))
 
 #define enum_item(kc, str, ...) kc,
 
