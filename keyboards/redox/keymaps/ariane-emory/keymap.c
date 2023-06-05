aeddde8bf5d7a64fb7083a31ac855780dcd58aa5
Ariane Emory <ariane.emory@protonmail.com>
2023-06-04 22:27:53 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -89,10 +89,10 @@ static uint16_t idle_timer = 0;
 // #define FOR_EACH_CTRLABLE_SEND_STRING_KEYCODE(DO)
  
 #define FOR_EACH_CTRLABLE_OR_ALTABLE_SEND_STRING_KEYCODE(DO)                                        \
-  DO(SS_ARROW,          ("->"),  ("`"),  (""))                                                      \
-  DO(SS_DIR,            ("../"), ("./"), ("~/"))                                                  \
-  DO(SS_LBRACK,         ("9"),   ("["),  ("{"))                                                 \
-  DO(SS_RBRACK,         ("0"),   ("]"),  ("}"))
+  DO(SS_ARROW,          ("->"),  ("`"SS_TAP(X_LEFT)"`"),  ("49"SS_TAP(X_LEFT)"9"))                  \
+  DO(SS_DIR,            ("../"), ("./"),                  ("~/"))                                   \
+  DO(SS_LBRACK,         ("9"),   ("["),                   ("{"))                                    \
+  DO(SS_RBRACK,         ("0"),   ("]"),                   ("}"))
 
 #define enum_item(kc, str, ...) kc,
 
