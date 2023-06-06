3f69422364807875567a54a4639467e2c873c598
Ariane Emory <ariane.emory@protonmail.com>
2023-06-05 20:31:39 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/features/rgb_fader.c
+++ keyboards/redox/keymaps/ariane-emory/features/rgb_fader.c
@@ -6,15 +6,15 @@
 #include <string.h>
 #endif
 
-#define COLORS                                                                  \
-  C(r)                                                                          \
-  C(g)                                                                          \
-  C(b)
+#define FOR_EACH_COLOR(DO)                                                      \
+  DO(r)                                                                         \
+  DO(g)                                                                         \
+  DO(b)
 
-#define RGBS                                                                    \
-  R(initial)                                                                    \
-  R(current)                                                                    \
-  R(target)
+#define FOR_EACH_RGB(DO)                                                        \
+  DO(initial)                                                                   \
+  DO(current)                                                                   \
+  DO(target)
 
 #ifndef RGB_FADER_NO_STRINGS
 void rgb_fader_update_c_str(rgb_fader_t * const this);
@@ -32,9 +32,7 @@ void rgb_fader_finish_init(rgb_fader_t * const this) {
 }
 
 bool rgb_fader_before_set_target (rgb_fader_t * const this, const rgb_t * const rgb) {
-  if (rgb_equal(&this->target, rgb))
-    return false; // return false, no change is needed.
-  return true;
+  return ! rgb_equal(&this->target, rgb);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -44,9 +42,9 @@ bool rgb_fader_before_set_target (rgb_fader_t * const this, const rgb_t * const 
 void rgb_fader_init(rgb_fader_t * const this, uint8_t r, uint8_t g, uint8_t b) {
   rgb_t rgb;
   rgb_init(&rgb, r, g, b);
-#define R(rr) rgb_copy(&this->rr, &rgb);
-  RGBS;
-#undef R
+#define copy_rgb(r) rgb_copy(&this->r, &rgb);
+  FOR_EACH_RGB(copy_rgb);
+#undef copy_rgb
   rgb_fader_finish_init(this);
 }
 
@@ -82,20 +80,20 @@ void rgb_fader_step(rgb_fader_t * const this) {
   if (UINT8_MAX == this->step) {
     rgb_fader_init(this, this->target.r, this->target.g, this->target.b);
   } else {
-#define C(color) const int color ## _delta = (this->target. color - this->initial. color);
-    COLORS;
-#undef C
+#define define_color_delta(color) const int color ## _delta = (this->target. color - this->initial. color);
+    FOR_EACH_COLOR(define_color_delta);
+#undef define_color_delta
     
     if (! (r_delta | g_delta | b_delta))
       return;
     
     this->step++;
 
-#define FMULS8(n, m)  (((n * m) >> 8) & 0xff)
-#define C(color) this->current. color = (uint8_t)(max(0, this->initial. color + FMULS8(color ## _delta, this->step)));
-    COLORS;
-#undef FMULS8
-#undef C
+#define fmuls8(n, m)  (((n * m) >> 8) & 0xff)
+#define set_current_color(color) this->current. color = (uint8_t)(max(0, this->initial. color + fmuls8(color ## _delta, this->step)));
+    FOR_EACH_COLOR(set_current_color);
+#undef fmuls8
+#undef set_current_color
 
 #ifndef RGB_FADER_NO_STRINGS
     rgb_fader_update_c_str(this);
@@ -121,13 +119,13 @@ void rgb_fader_printf(const rgb_fader_t * const this) {
 }
 
 void rgb_fader_describe(const rgb_fader_t * const this) {
-#define R(rgb)                                                                  \
+#define print_rgb_info(rgb)                                                     \
   printf("------------------------------\n");                                   \
   printf(#rgb ".r   = %3d\n", this->rgb.r);                                     \
   printf(#rgb ".g   = %3d\n", this->rgb.g);                                     \
   printf(#rgb ".b   = %3d\n", this->rgb.b);                                     \
-  RGBS;
-#undef R
+  FOR_EACH_RGB(print_rgb_info);
+#undef print_rgb_info
   
   printf("------------------------------\n");
   printf("step  = %3d\n", this->step);
@@ -168,5 +166,5 @@ bool rgb_fader_set_target_from_str(rgb_fader_t * const this, const char * const 
 }
 #endif
 
-#undef COLORS
-#undef RGBS
+#undef FOR_EACH_COLOR
+#undef FOR_EACH_RGB
