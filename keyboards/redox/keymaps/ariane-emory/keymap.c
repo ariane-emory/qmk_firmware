befc75134291454952242d5c8d6cb99bd39dd38b
Ariane Emory <ariane.emory@protonmail.com>
2023-06-16 01:40:50 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1,4 +1,4 @@
-// -*- c-backslash-column: 120; c-backslash-max-column: 120; -*-
+// -*- c-backslash-column: 128; c-backslash-max-column: 128 -*-
 
 #include QMK_KEYBOARD_H
 
@@ -83,20 +83,19 @@ static uint16_t idle_timer = 0;
 #define TB SS_TAP(X_TAB)
 #define CR SS_TAP(X_ENT)
 
-#define FOR_EACH_SHIFTABLE_OR_ALTABLE_SEND_STRING_KEYCODE(DO)                                                           \
-  DO(SS_SIRI,           (SS_DOWN(X_F24)SS_DELAY(50)SS_TAP(X_SPC)SS_UP(X_F24)),   (""),                   (""))          \
-  DO(SS_DICT,           (SS_TAP(X_F24)SS_TAP(X_F24)),                            (""),                   (""))          \
-  DO(EM_CHG_BUF,        (SS_LCTL("x")SS_DELAY(50)"b"),                           (""),                   (""))          \
-  DO(EM_KIL_BUF,        (SS_LCTL("x")SS_DELAY(50)SS_LCTL("k")),                  (""),                   (""))          \
-  DO(EM_REVERT,         (SS_LCTL("x")SS_DELAY(50)SS_LCTL("r")),                  (""),                   (""))          \
-  DO(EM_LASTARG,        (" "SS_LCTL("c")SS_DELAY(50)"."),                        (""),                   (""))          \
-  DO(SS_BANGBANG,       ("11"SS_TAP(X_ENT)),                                     (""),                   (""))          \
-  DO(SS_PIN1,           (AE_PIN1),                                               (AE_PIN2),              (""))          \
-  DO(SS_ARROW,          ("->"),                                                  ("490"LL),              ("``"LL))      \
-  DO(SS_DIR,            ("~/"),                                                  ("../"),                ("./"))        \
-  DO(SS_LBRACK,         ("9"),                                                   ("{"),                  ("["))         \
-  DO(SS_RBRACK,         ("0"),                                                   ("}"),                  ("]"))         \
-  DO(SS_BRACKS,         ("90" LL),                                               ("{" CR CR "}" UU TB),  ("[]" LL))
+#define FOR_EACH_SHIFTABLE_OR_ALTABLE_SEND_STRING_KEYCODE(DO)                                                                   \
+  DO(SS_DICT,      (SS_TAP(X_F24)SS_TAP(X_F24)),            (SS_DOWN(X_F24)SS_DELAY(50)SS_TAP(X_SPC)SS_UP(X_F24)),   (""))      \
+  DO(EM_CHG_BUF,   (SS_LCTL("x")SS_DELAY(50)"b"),           (""),                                                    (""))      \
+  DO(EM_KIL_BUF,   (SS_LCTL("x")SS_DELAY(50)SS_LCTL("k")),  (""),                                                    (""))      \
+  DO(EM_REVERT,    (SS_LCTL("x")SS_DELAY(50)SS_LCTL("r")),  (""),                                                    (""))      \
+  DO(EM_LASTARG,   (" "SS_LCTL("c")SS_DELAY(50)"."),        (""),                                                    (""))      \
+  DO(SS_BANGBANG,  ("11"SS_TAP(X_ENT)),                     (""),                                                    (""))      \
+  DO(SS_PIN1,      (AE_PIN1),                               (AE_PIN2),                                               (""))      \
+  DO(SS_ARROW,     ("->"),                                  ("490"LL),                                               ("``"LL))  \
+  DO(SS_DIR,       ("~/"),                                  ("../"),                                                 ("./"))    \
+  DO(SS_LBRACK,    ("9"),                                   ("{"),                                                   ("["))     \
+  DO(SS_RBRACK,    ("0"),                                   ("}"),                                                   ("]"))     \
+  DO(SS_BRACKS,    ("90" LL),                               ("{" CR CR "}" UU TB),                                   ("[]" LL))
 
 #define enum_item(kc, str, ...)                                                  kc,
 #define define_tagged_progmem_string(tag, kc, str, ...)                          static const char tag##_str_##kc[] PROGMEM = str;
