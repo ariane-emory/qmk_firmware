3fa9a73bb9a2fe4964a50172c4f898e766e4e0ca
Ariane Emory <ariane.emory@protonmail.com>
2023-06-16 22:03:35 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -50,7 +50,7 @@
 #define PERMISSIVE_HOLD_PER_KEY
 
 #define TAPPING_TERM                         200
-#define TAPPING_TERM_PER_KEY
+// #define TAPPING_TERM_PER_KEY
 
 #define TOGGLED_LAYER                        6
 #define TOGGLED_LAYER_TIMEOUT                30000
