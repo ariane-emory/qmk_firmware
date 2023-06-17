68ce1b81d03372f1b8ab551e594a187a1ccddf32
Ariane Emory <ariane.emory@protonmail.com>
2023-06-16 21:57:41 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -632,7 +632,7 @@ process_bilaterally:
   // Also allow same-hand holds when the other key is in the rows below the
   // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
   if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4)
-        return true;
+    return true;
   
   // Require bilateral
   return achordion_opposite_hands(tap_hold_record, other_record);
