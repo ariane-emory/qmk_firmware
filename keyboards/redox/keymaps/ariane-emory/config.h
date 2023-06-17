e8b17cd4e8605745c73d8a436a61e0842df2ec16
Ariane Emory <ariane.emory@protonmail.com>
2023-06-16 21:36:53 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -32,7 +32,7 @@
 
 #define COMBO_ONLY_FROM_LAYER                0
 #define COMBO_SHOULD_TRIGGER
-#define COMBO_TERM                           40
+#define COMBO_TERM                           66
 
 #define MOUSEKEY_DELAY                       0
 #define MOUSEKEY_INTERVAL                    21 // 20
@@ -49,7 +49,73 @@
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 #define PERMISSIVE_HOLD_PER_KEY
 
-#define TAPPING_TERM                         250
+#define TAPPING_TERM                         200
+#define TAPPING_TERM_PER_KEY
+
+#define TOGGLED_LAYER                        6
+#define TOGGLED_LAYER_TIMEOUT                30000
+
+#define MY_RGB_DEFAULT                       RGB_ORANGE
+#define MY_RGB_RECORDING_MACRO               RGB_MAGENTA
+#define MY_RGB_TOGGLED_LAYER_ON              RGB_CHARTREUSE
+#define MY_RGB_LOWER_LAYER_ON                RGB_CYAN
+#define MY_RGB_UPPER_LAYER_ON                RGB_BLUE
+#define MY_RGB_ADJUST_LAYER_ON               RGB_PURPLE
+
+#define TRI_LAYER_LOWER_LAYER                7
+#define TRI_LAYER_UPPER_LAYER                8
+#define TRI_LAYER_ADJUST_LAYER               13
+
+#define HOME_ROW_MODS
+// #define BOTTOM_ROW_MODS
+#define MY_RGB_LAYERS
+
+#define NO_ACTION_ONESHOT
+#define RGB_FADER_NO_STRINGS
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+#undef EEPROM_ENABLE
+#undef EEPROM_DRIVER
+
+#ifdef DYNAMIC_MACRO_ENABLE
+#    define DYNAMIC_MACRO_DELAY              15
+#endif
+
+// #define STAGGERED_SHIFT
+#define TOP_SHIFT
+#define BOTTOM_SHIFT
+
+#define MY_RGB_RECORDING_MACRO               RGB_MAGENTA
+#define MY_RGB_TOGGLED_LAYER_ON              RGB_CHARTREUSE
+#define MY_RGB_LOWER_LAYER_ON                RGB_CYAN
+#define MY_RGB_UPPER_LAYER_ON                RGB_BLUE
+#define MY_RGB_ADJUST_LAYER_ON               RGB_PURPLE
+
+#define TRI_LAYER_LOWER_LAYER                7
+#define TRI_LAYER_UPPER_LAYER                8
+#define TRI_LAYER_ADJUST_LAYER               13
+
+#define HOME_ROW_MODS
+// #define BOTTOM_ROW_MODS
+#define MY_RGB_LAYERS
+
+#define NO_ACTION_ONESHOT
+#define RGB_FADER_NO_STRINGS
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+#undef EEPROM_ENABLE
+#undef EEPROM_DRIVER
+
+#ifdef DYNAMIC_MACRO_ENABLE
+#    define DYNAMIC_MACRO_DELAY              15
+#endif
+
+// #define STAGGERED_SHIFT
+#define TOP_SHIFT
+#define BOTTOM_SHIFT
+
 #define TAPPING_TERM_PER_KEY
 
 #define TOGGLED_LAYER                        6
