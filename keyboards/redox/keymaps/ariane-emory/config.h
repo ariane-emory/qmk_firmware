b5bf6a541198dfd7ff98d8db50b05dee3fc0ae1c
Ariane Emory <ariane.emory@protonmail.com>
2023-06-18 21:19:19 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -26,7 +26,7 @@
 // #define AUTO_SHIFT_MODIFIERS
 #define AUTO_SHIFT_REPEAT
 #define AUTO_SHIFT_TIMEOUT                   180
-#define AUTO_SHIFT_TIMEOUT_PER_KEY
+// #define AUTO_SHIFT_TIMEOUT_PER_KEY
 #define NO_AUTO_SHIFT_ALPHA
 // #define RETRO_TAPPING
 
