b285cdf2f6432f58c909561728513223c985230b
Ariane Emory <ariane.emory@protonmail.com>
2023-06-18 21:13:04 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -629,7 +629,6 @@ KEYRECORD_FUN(get_permissive_hold, bool) {
   if (IS_LAYER_ON(0)) {
     switch (keycode) {
     case MT(MOD_LALT,KC_SPC):
-    case MT(MOD_RGUI,KC_SPC):
 #ifdef HOME_ROW_MODS
     case QH_A: case QH_S: case QH_D: case QH_F:
       // case QH_G: case QH_H: // not mods currently
