2be2f9a3f29bf9f94445216a9b4a248e6b43492d
Ariane Emory <ariane.emory@protonmail.com>
2023-06-23 23:55:27 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -108,8 +108,8 @@ static uint16_t idle_timer = 0;
   DO(SS_LBRACK,    ("9"),                                   ("["),                                                   ("{"))     \
   DO(SS_RBRACK,    ("0"),                                   ("]"),                                                   ("}"))     \
   DO(SS_BRACKS,    ("90" LL),                               ("[" CR CR "]" UU TB),                                   ("{}" LL)) \
-  DO(SS_SMILEY,    (" :0"),                                  (" ;0"),                                                (" :P"))   \
-  DO(SS_SMILEY2,   (" :/"),                                  (" :D"),                                                (" :9"))
+  DO(SS_SMILEY,    (" :0"),                                  (" :/"),                                                (" :D"))   \
+  DO(SS_SMILEY2,   (" ;0"),                                  (" :P"),                                                (" :9"))
   
 #define enum_item(kc, str, ...)                                                     kc,
 #define define_tagged_progmem_string(tag, kc, str, ...)                             static const char tag##_str_##kc[] PROGMEM = str;
