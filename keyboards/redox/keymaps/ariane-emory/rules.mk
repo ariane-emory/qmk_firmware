abeb9dd9ea61d8fd5e37bfbe966ec7dc41c9cf1c
Ariane Emory <ariane.emory@protonmail.com>
2023-06-28 03:02:45 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/rules.mk
+++ keyboards/redox/keymaps/ariane-emory/rules.mk
@@ -1,39 +1,31 @@
-AUTO_SHIFT_ENABLE  	 	 = no
-BOOTMAGIC_ENABLE       = no
-COMMAND_ENABLE     	 	 = no
-CONSOLE_ENABLE     	 	 = no
-GRAVE_ESC_ENABLE   	 	 = no 
-MAGIC_ENABLE       	 	 = no
-SPACE_CADET_ENABLE 	 	 = no
-TAP_DANCE_ENABLE   	 	 = no
+AUTO_SHIFT_ENABLE  	 	 			 = no
+BOOTMAGIC_ENABLE       			 = no
+COMMAND_ENABLE     	 	 			 = no
+CONSOLE_ENABLE     	 	 			 = no
+GRAVE_ESC_ENABLE   	 	 			 = no 
+MAGIC_ENABLE       	 	 			 = no
+SPACE_CADET_ENABLE 	 	 			 = no
+TAP_DANCE_ENABLE   	 	 			 = no
 
-AVR_USE_MINIMAL_PRINTF = yes
-CAPS_WORD_ENABLE   	 	 = yes
-COMBO_ENABLE       	 	 = yes
-LTO_ENABLE         	 	 = yes
-NKRO_ENABLE        	 	 = yes
-RGBLIGHT_ENABLE    	 	 = yes 
-SEND_STRING_ENABLE 	 	 = yes
-TRI_LAYER_ENABLE   	 	 = no
-DEFERRED_EXEC_ENABLE 	 = yes
-DYNAMIC_MACRO_ENABLE   = yes
-DYNAMIC_TAPPING_TERM_ENABLE = no
+AVR_USE_MINIMAL_PRINTF 			 = yes
+CAPS_WORD_ENABLE   	 	 			 = yes
+COMBO_ENABLE       	 	 			 = yes
+LTO_ENABLE         	 	 			 = yes
+NKRO_ENABLE        	 	 			 = yes
+RGBLIGHT_ENABLE    	 	 			 = yes 
+SEND_STRING_ENABLE 	 	 			 = yes
+TRI_LAYER_ENABLE   	 	 			 = no
+DEFERRED_EXEC_ENABLE 	 			 = yes
+DYNAMIC_MACRO_ENABLE   			 = yes
+DYNAMIC_TAPPING_TERM_ENABLE  = no
 
-SRC                   += src/achordion.c
-SRC                   += src/cRGB_functions.c
-SRC                   += src/cRGB_fader.c
-SRC                   += src/util.c
-SRC                   += src/tap_number.c
+SRC                         += src/achordion.c
+SRC                         += src/cRGB_functions.c
+SRC                         += src/cRGB_fader.c
+SRC                         += src/util.c
+SRC                         += src/tap_number.c
 
-CFLAGS                += -funroll-loops
-
-TOP_SYMBOLS            = 15
-
-# EEPROM_DRIVER          = transient
-
-
-
-TOP_SYMBOLS            = 15
-
-# EEPROM_DRIVER          = transient
+CFLAGS                      += -funroll-loops
+TOP_SYMBOLS                  = 15
+# EEPROM_DRIVER              = transient
 
