066258f6ee6d5cea016b3bf1fd52f294cc795368
Ariane Emory <ariane.emory@protonmail.com>
2023-08-24 13:50:46 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -88,8 +88,8 @@ void send_string_without_mods_P(const char * const string) {
 
 #define FOR_EACH_SHIFTABLE_OR_CTRLABLE_SEND_STRING_KEYCODE(DO)                                                                                                       \
   DO(SS_TELEPORT,          (TELEPORT_RAW),                                      (""),                                                  (""))                         \
-  DO(GUI_CLICK,            (GUI_CLICK_RAW),                                     (TELEPORT_RAW),                                        (""))                         \
-  DO(GUI_CLICK_AND_TAB,    (GUI_CLICK_AND_TAB_STR),                             (""),                                                  (""))                         \
+  DO(SS_GUI_CLICK,         (GUI_CLICK_RAW),                                     (TELEPORT_RAW),                                        (""))                         \
+  DO(SS_GUI_CLICK_AND_TAB, (GUI_CLICK_AND_TAB_STR),                             (""),                                                  (""))                         \
   DO(SS_FULLSCR,           (SS_DOWN(X_F24) DD SS_TAP(X_F) SS_UP(X_F24)),        (""),                                                  (""))                         \
   DO(SS_DICT,              (SS_TAP(X_F24) SS_TAP(X_F24)),                       (SS_DOWN(X_F24) DD SS_TAP(X_SPC) SS_UP(X_F24)),        (""))                         \
   DO(EM_CHG_BUF,           (SS_LCTL("x") DD "b"),                               (""),                                                  (""))                         \
