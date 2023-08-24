9686c2cdd41fdafbaf4ecee0dbfc5859d337ed52
Ariane Emory <ariane.emory@protonmail.com>
2023-08-24 16:48:30 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -588,7 +588,8 @@ void leader_end_user(void) {
   }
   else if (leader_sequence_two_keys(KC_G, KC_R)) {
     SEND_STRING_WITHOUT_MODS_P(PSTR("git reset "));
-  } else {
+  }
+  else if (leader_sequence_one_key(0)) {
     tap_code16(LCA(KC_X));
   }
 }
