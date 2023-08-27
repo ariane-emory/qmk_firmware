d8a58dd08664a0b7731e211a93fbe9ed69efddd1
Ariane Emory <ariane.emory@protonmail.com>
2023-08-27 09:22:21 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -80,13 +80,13 @@ void send_string_without_mods_P(const char * const string) {
 #define UU                 SS_TAP(X_UP) 
 #define TB                 SS_TAP(X_TAB)
 #define CR                 SS_TAP(X_ENT)
-#define SCR_L              SS_LCTL(SS_TAP(X_F13))
-#define SCR_R              SS_LCTL(SS_TAP(X_F14))
+#define SCR_L()            SS_LCTL(SS_TAP(X_F13))
+#define SCR_R()            SS_LCTL(SS_TAP(X_F14))
 
 #define GUI_CLICK(_)       SS_DOWN(X_LGUI) DD() SS_TAP(X_BTN1) DD() SS_UP(X_LGUI)
 #define GUI_CLICK_AND_TAB  GUI_CLICK() SS_LCTL(SS_TAP(X_TAB))
 #define REPEAT_SHELL_CMD   SS_DOWN(X_LGUI) SS_TAP(X_A) SS_UP(X_LGUI) SS_TAP(X_BSPC) SS_TAP(X_ENT) "11" SS_TAP(X_ENT)
-#define TELEPORT           GUI_CLICK() DD() SS_LCTL(SS_TAP(X_TAB)) DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("x") DD() SS_LGUI("w") DD() SS_LGUI("`") DD() SS_TAP(X_ESC) DD() SS_LGUI("t") DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("v") DD() SS_TAP(X_ENT) DD() SS_TAP(X_F) DD() SS_DELAY(1200) DD() SCR_R SS_TAP(X_BTN1) SS_TAP(X_F) SS_TAP(X_BTN1) SS_DELAY(300) RR RR RR RR RR RR RR SS_LGUI("`") SCR_L DD()
+#define TELEPORT           GUI_CLICK() DD() SS_LCTL(SS_TAP(X_TAB)) DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("x") DD() SS_LGUI("w") DD() SS_LGUI("`") DD() SS_TAP(X_ESC) DD() SS_LGUI("t") DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("v") DD() SS_TAP(X_ENT) DD() SS_TAP(X_F) DD() SS_DELAY(1200) DD() SCR_R() SS_TAP(X_BTN1) SS_TAP(X_F) SS_TAP(X_BTN1) SS_DELAY(300) RR RR RR RR RR RR RR SS_LGUI("`") SCR_L() DD()
 
 #define FOR_EACH_SHIFTABLE_OR_CTRLABLE_SEND_STRING_KEYCODE(DO)                                                                                        \
   DO(SS_TELEPORT,          (TELEPORT),                                          (""),                                                  (""))          \
