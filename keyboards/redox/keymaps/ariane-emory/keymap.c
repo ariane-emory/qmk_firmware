b9cb101257f81339051f034e4ed4366e45302cc4
Ariane Emory <ariane.emory@protonmail.com>
2023-08-27 09:05:25 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -79,26 +79,26 @@ void send_string_without_mods_P(const char * const string) {
 #define UU                 SS_TAP(X_UP) 
 #define TB                 SS_TAP(X_TAB)
 #define CR                 SS_TAP(X_ENT)
-#define DD                 SS_DELAY(0)
+#define DD(_)              SS_DELAY(0)
 #define SCR_L              SS_LCTL(SS_TAP(X_F13))
 #define SCR_R              SS_LCTL(SS_TAP(X_F14))
 
 #define REPEAT_SHELL_CMD   SS_DOWN(X_LGUI) SS_TAP(X_A) SS_UP(X_LGUI) SS_TAP(X_BSPC) SS_TAP(X_ENT) "11" SS_TAP(X_ENT)
-#define GUI_CLICK          SS_DOWN(X_LGUI) DD SS_TAP(X_BTN1) DD SS_UP(X_LGUI)
-#define TELEPORT           GUI_CLICK DD SS_LCTL(SS_TAP(X_TAB)) DD SS_LGUI("l") DD SS_LGUI("a") DD SS_LGUI("x") DD SS_LGUI("w") DD SS_LGUI("`") DD SS_TAP(X_ESC) DD SS_LGUI("t") DD SS_LGUI("l") DD SS_LGUI("a") DD SS_LGUI("v") DD SS_TAP(X_ENT) DD SS_TAP(X_F) DD SS_DELAY(1200) DD SCR_R SS_TAP(X_BTN1) SS_TAP(X_F) SS_TAP(X_BTN1) SS_DELAY(300) RR RR RR RR RR RR RR SS_LGUI("`") SCR_L DD
+#define GUI_CLICK          SS_DOWN(X_LGUI) DD() SS_TAP(X_BTN1) DD() SS_UP(X_LGUI)
+#define TELEPORT           GUI_CLICK DD() SS_LCTL(SS_TAP(X_TAB)) DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("x") DD() SS_LGUI("w") DD() SS_LGUI("`") DD() SS_TAP(X_ESC) DD() SS_LGUI("t") DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("v") DD() SS_TAP(X_ENT) DD() SS_TAP(X_F) DD() SS_DELAY(1200) DD() SCR_R SS_TAP(X_BTN1) SS_TAP(X_F) SS_TAP(X_BTN1) SS_DELAY(300) RR RR RR RR RR RR RR SS_LGUI("`") SCR_L DD()
 #define GUI_CLICK_AND_TAB  GUI_CLICK SS_LCTL(SS_TAP(X_TAB))
 
 #define FOR_EACH_SHIFTABLE_OR_CTRLABLE_SEND_STRING_KEYCODE(DO)                                                                                                       \
   DO(SS_TELEPORT,          (TELEPORT),                                          (""),                                                  (""))                         \
   DO(SS_GUI_CLICK,         (GUI_CLICK),                                         (TELEPORT),                                            (""))                         \
   DO(SS_GUI_CLICK_AND_TAB, (GUI_CLICK_AND_TAB),                                 (""),                                                  (""))                         \
-  DO(SS_FULLSCR,           (SS_DOWN(X_F24) DD SS_TAP(X_F) SS_UP(X_F24)),        (""),                                                  (""))                         \
-  DO(SS_DICT,              (SS_TAP(X_F24) SS_TAP(X_F24)),                       (SS_DOWN(X_F24) DD SS_TAP(X_SPC) SS_UP(X_F24)),        (""))                         \
-  DO(EM_CHG_BUF,           (SS_LCTL("x") DD "b"),                               (""),                                                  (""))                         \
-  DO(EM_KIL_BUF,           (SS_LCTL("x") DD SS_LCTL("k")),                      (""),                                                  (""))                         \
-  DO(EM_REVERT,            (SS_LCTL("x") DD SS_LCTL("r")),                      (""),                                                  (""))                         \
-  DO(EM_LASTARG,           (" " SS_LCTL("c") DD "."),                           (""),                                                  (""))                         \
-  DO(SS_BANGBANG,          (REPEAT_SHELL_CMD),                                  ("11" SS_TAP(X_ENT)),                                  (""))                         \
+  DO(SS_FULLSCR,           (SS_DOWN(X_F24) DD() SS_TAP(X_F) SS_UP(X_F24)),        (""),                                                  (""))        \
+  DO(SS_DICT,              (SS_TAP(X_F24) SS_TAP(X_F24)),                       (SS_DOWN(X_F24) DD() SS_TAP(X_SPC) SS_UP(X_F24)),        (""))        \
+  DO(EM_CHG_BUF,           (SS_LCTL("x") DD() "b"),                               (""),                                                  (""))        \
+  DO(EM_KIL_BUF,           (SS_LCTL("x") DD() SS_LCTL("k")),                      (""),                                                  (""))        \
+  DO(EM_REVERT,            (SS_LCTL("x") DD() SS_LCTL("r")),                      (""),                                                  (""))        \
+  DO(EM_LASTARG,           (" " SS_LCTL("c") DD() "."),                           (""),                                                  (""))        \
+  DO(SS_BANGBANG,          (REPEAT_SHELL_CMD),                                  ("11" SS_TAP(X_ENT)),                                  (""))          \
   DO(SS_PIN1,              (AE_PIN1),                                           (AE_PIN2),                                             (ROUTER_PWD))                 \
   DO(SS_ARROW,             ("->"),                                              (" => "),                                              ("490" LL))                   \
   DO(SS_DIR,               ("~/"),                                              ("../"),                                               ("./"))                       \
