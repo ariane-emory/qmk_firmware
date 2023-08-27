085f96e1c518cb0ff2ed829163082f92845f01e3
Ariane Emory <ariane.emory@protonmail.com>
2023-08-27 09:13:23 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -84,13 +84,13 @@ void send_string_without_mods_P(const char * const string) {
 #define SCR_R              SS_LCTL(SS_TAP(X_F14))
 
 #define REPEAT_SHELL_CMD   SS_DOWN(X_LGUI) SS_TAP(X_A) SS_UP(X_LGUI) SS_TAP(X_BSPC) SS_TAP(X_ENT) "11" SS_TAP(X_ENT)
-#define GUI_CLICK          SS_DOWN(X_LGUI) DD() SS_TAP(X_BTN1) DD() SS_UP(X_LGUI)
-#define GUI_CLICK_AND_TAB  GUI_CLICK SS_LCTL(SS_TAP(X_TAB))
-#define TELEPORT           GUI_CLICK DD() SS_LCTL(SS_TAP(X_TAB)) DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("x") DD() SS_LGUI("w") DD() SS_LGUI("`") DD() SS_TAP(X_ESC) DD() SS_LGUI("t") DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("v") DD() SS_TAP(X_ENT) DD() SS_TAP(X_F) DD() SS_DELAY(1200) DD() SCR_R SS_TAP(X_BTN1) SS_TAP(X_F) SS_TAP(X_BTN1) SS_DELAY(300) RR RR RR RR RR RR RR SS_LGUI("`") SCR_L DD()
+#define GUI_CLICK(_)       SS_DOWN(X_LGUI) DD() SS_TAP(X_BTN1) DD() SS_UP(X_LGUI)
+#define GUI_CLICK_AND_TAB  GUI_CLICK() SS_LCTL(SS_TAP(X_TAB))
+#define TELEPORT           GUI_CLICK() DD() SS_LCTL(SS_TAP(X_TAB)) DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("x") DD() SS_LGUI("w") DD() SS_LGUI("`") DD() SS_TAP(X_ESC) DD() SS_LGUI("t") DD() SS_LGUI("l") DD() SS_LGUI("a") DD() SS_LGUI("v") DD() SS_TAP(X_ENT) DD() SS_TAP(X_F) DD() SS_DELAY(1200) DD() SCR_R SS_TAP(X_BTN1) SS_TAP(X_F) SS_TAP(X_BTN1) SS_DELAY(300) RR RR RR RR RR RR RR SS_LGUI("`") SCR_L DD()
 
 #define FOR_EACH_SHIFTABLE_OR_CTRLABLE_SEND_STRING_KEYCODE(DO)                                                                                        \
   DO(SS_TELEPORT,          (TELEPORT),                                          (""),                                                  (""))          \
-  DO(SS_GUI_CLICK,         (GUI_CLICK),                                         (TELEPORT),                                            (""))          \
+  DO(SS_GUI_CLICK,         (GUI_CLICK()),                                       (TELEPORT),                                            (""))          \
   DO(SS_GUI_CLICK_AND_TAB, (GUI_CLICK_AND_TAB),                                 (""),                                                  (""))          \
   DO(SS_FULLSCR,           (SS_DOWN(X_F24) DD() SS_TAP(X_F) SS_UP(X_F24)),      (""),                                                  (""))          \
   DO(SS_DICT,              (SS_TAP(X_F24) SS_TAP(X_F24)),                       (SS_DOWN(X_F24) DD() SS_TAP(X_SPC) SS_UP(X_F24)),      (""))          \
@@ -548,10 +548,10 @@ uint16_t keycode_config(uint16_t keycode) {
 
 void leader_end_user(void) {
   if (leader_sequence_one_key(KC_Q)) {
-    SEND_STRING_WITHOUT_MODS_P(PSTR("qmk compile" ENT));
+    SEND_STRING_WITHOUT_MODS_P(PSTR("qmkc" ENT));
   }
   else if (leader_sequence_two_keys(KC_Q, KC_S)) {
-    SEND_STRING_WITHOUT_MODS_P(PSTR("qmk compile; shove" ENT));
+    SEND_STRING_WITHOUT_MODS_P(PSTR("qmkc; shove" ENT));
   }
   else if (leader_sequence_one_key(KC_S)) {
     SEND_STRING_WITHOUT_MODS_P(PSTR("shove" ENT));
