34ed83f028033a4ec96a61da3aac17d173f9cd58
Ariane Emory <ariane.emory@protonmail.com>
2023-11-19 21:15:27 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/src/key-aliases.h
+++ keyboards/redox/keymaps/ariane-emory/src/key-aliases.h
@@ -288,8 +288,8 @@
 #define KT_P    KC_P
 //------------------------------------------------------------------------------
 #define KH_A    LHRM_4(KC_A)
-#define KH_S    LHRM_3(KC_R)
-#define KH_D    LHRM_2(KC_S)
+#define KH_S    LHRM_3(KC_S)
+#define KH_D    LHRM_2(KC_R)
 #define KH_F    LHRM_1(KC_T)
 #define KH_G    LHRM_0(KC_G)
 #define KH_H    RHRM_0(KC_C)
