bf80886bbbf35efeb98e420bdd2baa7e62d9e3ec
Ariane Emory <ariane.emory@protonmail.com>
2023-11-24 11:39:08 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -119,7 +119,6 @@ void send_string_without_mods_P(const char * const string) {
   DO(SS_DIR,               ("~/"),                               ("../"),                                ("./"))                                      \
   DO(SS_LBRACK,            ("9"),                                ("["),                                  ("{"))                                       \
   DO(SS_RBRACK,            ("0"),                                ("]"),                                  ("}"))                                       \
-  DO(SS_BRACKS,            ("90" LL()),                          ("[" CR() CR() "]" UU() TB()),          ("{}" LL()))                                 \
   DO(SS_SMILEY,            (" :0"),                              (" :/"),                                (" >_>"))                                    \
   DO(SS_SMILEY2,           (" ;0"),                              (" :P"),                                (" :D"))                                     \
   DO(SS_AND_AND,           (" 77 "),                             (" || "),                               (" @>7! "))                                  \
