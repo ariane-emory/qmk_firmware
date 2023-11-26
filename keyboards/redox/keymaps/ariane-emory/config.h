ba599c156c7915f2d95fb00249c37269520dd337
Ariane Emory <ariane.emory@protonmail.com>
2023-11-26 17:07:58 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -64,7 +64,7 @@
 #define MY_RGB_UPPER_LAYER_ON                RGB_MAGENTA
 #define MY_RGB_ADJUST_LAYER_ON               0xFF, 0x38, 0x00 // orange-y
 #define MY_RGB_TOGGLED_LAYER_ON              RGB_CHARTREUSE
-#define MY_RGB_BOOT                          0x0,  0xFF, 0x30
+#define MY_RGB_BOOT                          0x0,  0xFF, 0x28
 
 #define NO_ACTION_ONESHOT
 #define ONESHOT_TIMEOUT                      1000
