6c116324bcf09de8bb15461cea3ba32e3d61f0c8
Ariane Emory <ariane.emory@protonmail.com>
2023-12-02 17:01:46 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -184,15 +184,15 @@ KEYRECORD_C_FUN(bool process_moddable_send_string) {
   for (uint8_t ix = 0; ix < ARRAY_SIZE(moddable_send_string_keycodes); ix++) {
     if (moddable_send_string_keycodes[ix].kc == keycode) {      
       if (record->event.pressed)  {
-        if      ((pgm_read_byte(moddable_send_string_keycodes[ix].ctrled_str) != 0) &&
+        if      ((moddable_send_string_keycodes[ix].ctrled_str != 0) &&
                  (get_mods() & MOD_MASK_CTRL)) {
           SEND_STRING_WITHOUT_MODS_P(moddable_send_string_keycodes[ix].ctrled_str);
         }
-        else if ((pgm_read_byte(moddable_send_string_keycodes[ix].alted_str) != 0) &&
+        else if ((moddable_send_string_keycodes[ix].alted_str != 0) &&
                  (get_mods() & MOD_MASK_ALT)) {
           SEND_STRING_WITHOUT_MODS_P(moddable_send_string_keycodes[ix].alted_str);
         }
-        else if ((pgm_read_byte(moddable_send_string_keycodes[ix].shifted_str) != 0) &&
+        else if ((moddable_send_string_keycodes[ix].shifted_str != 0) &&
                  (get_mods() & MOD_MASK_SHIFT)) {
           SEND_STRING_WITHOUT_MODS_P(moddable_send_string_keycodes[ix].shifted_str);
         }
