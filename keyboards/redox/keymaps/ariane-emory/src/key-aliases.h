00993b30644d31f513ea8994b3181431f3973d0d
Ariane Emory <ariane.emory@protonmail.com>
2023-12-01 20:26:42 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/src/key-aliases.h
+++ keyboards/redox/keymaps/ariane-emory/src/key-aliases.h
@@ -109,8 +109,8 @@
 #define RCTL_SQUO      RCTL_T(KC_SQUO_TAP)
 #define RCTL_DQUO      RCTL_T(KC_DQUO_TAP)
 #define LCTL_ESC       LCTL_T(KC_ESC)
-#define BRTU           KC_VOLU
-#define BRTD           KC_VOLD
+#define TMPU           KC_VOLU
+#define TMPD           KC_VOLD
 #define VOLU           KC_F20
 #define VOLD           KC_F19
 #define TG_MOUSE       TG(LN_MOUSE)
