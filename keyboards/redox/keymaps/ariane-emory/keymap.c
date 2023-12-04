e01a746bb1c5f7dfa88f43c1d23084450e0605bd
Ariane Emory <ariane.emory@protonmail.com>
2023-12-04 16:38:06 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -293,7 +293,8 @@ static const struct { uint16_t keycode; keycode_handler_fun_t handler; } keycode
   { OTHER_WIN,               other_win_handler           },
   { TOGGLE_DF,               toggle_df_handler           },
   { KC_LOWER,                disable_mouse_layer_handler },
-  { FLIPL_OR_TAB,            disable_mouse_layer_handler },
+  { CRL_LFT,                 disable_mouse_layer_handler },
+  { CRL_MS,                  disable_mouse_layer_handler },
   { DISCORD_MUTE,            discord_mute_handler        },
   { MY_BOOT,                 my_boot_handler             },
 #ifdef DYNAMIC_MACRO_HANDLERS
