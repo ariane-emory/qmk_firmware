105419913c7bb2536999939f279cfaf8a6df0f49
Ariane Emory <ariane.emory@protonmail.com>
2024-01-05 00:58:09 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/src/layouts.h
+++ keyboards/redox/keymaps/ariane-emory/src/layouts.h
@@ -93,8 +93,8 @@
 #define YB_X    KC_M
 #define YB_C    KC_L
 #define YB_V    LBRM_4(KC_B) 
-#define YB_B    KC_V
-#define YB_N    KC_K
+#define YB_B    KC_K
+#define YB_N    KC_V
 #define YB_M    LBRM_4(KC_X)
 #define YB_COMM KC_COMM
 #define YB_DOT  KC_DOT
