ba08dd3f91f2651d316e088fc489eceb64772430
Ariane Emory <ariane.emory@protonmail.com>
2024-01-05 22:12:57 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -761,7 +761,7 @@ void leader_end_user(void) {
   if (leader_sequence_one_key(KC_R)) {
     SEND_STRING_WITHOUT_MODS_P(PSTR(REPEAT_SHELL_CMD(_)));
   }
-  else if (leader_sequence_one_key(KC_B)) {
+  else if (leader_sequence_two_keys(KC_B, KC_B)) {
     my_boot_handler(0, NULL);
   }
   else if (leader_sequence_one_key(KC_Q)) {
