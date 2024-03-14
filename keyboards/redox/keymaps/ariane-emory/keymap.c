8b1a4cadc6530b0fcd2684dcfd83d48c612d97b2
Ariane Emory <ariane.emory@protonmail.com>
2024-03-13 23:52:27 -0400

    wip

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -111,7 +111,8 @@ void keyboard_post_init_user(void) {
                                                                  T(X_F) DD() RR() DD() RR() DD()                                                      \
                                                                         SS_LGUI("`") DD() SCR_L()
 
-#define FOR_EACH_MODDABLE_SEND_STRING_KEYCODE(DO)                                                                                                     \
+#ifdef AE_FLIPPED_NUMS
+  #define FOR_EACH_MODDABLE_SEND_STRING_KEYCODE(DO)                                                                                                     \
   DO(SS_TELEPORT,          (TELEPORT()),                         (""),                                   (""),               (""))                    \
   DO(SS_FULLSCR,           (SS_DOWN(X_F24) T(X_F) SS_UP(X_F24)), (""),                                   (""),               (""))                    \
   DO(EM_CHG_BUFF,          (SS_LCTL("x") "b"),                   (""),                                   (""),               (""))                    \
@@ -132,6 +133,29 @@ void keyboard_post_init_user(void) {
   DO(SS_RBRACK,            ("0"),                                ("}"),                                  ("]"),              (">"))                   \
   DO(SS_AND_AND,           (" 77 "),                             (" || "),                               (" @>7! "),         (" >/dev/null "))        \
 //                         NO MODS                               CTRL                                    ALT                 SHIFT
+#else
+  #define FOR_EACH_MODDABLE_SEND_STRING_KEYCODE(DO)                                                                                                   \
+  DO(SS_TELEPORT,          (TELEPORT()),                         (""),                                   (""),               (""))                    \
+  DO(SS_FULLSCR,           (SS_DOWN(X_F24) T(X_F) SS_UP(X_F24)), (""),                                   (""),               (""))                    \
+  DO(EM_CHG_BUFF,          (SS_LCTL("x") "b"),                   (""),                                   (""),               (""))                    \
+  DO(EM_KILL_BUFF,         (SS_LCTL("x") SS_LCTL("k")),          (""),                                   (""),               (""))                    \
+  DO(EM_REVERT,            (SS_LCTL("x") SS_LCTL("r")),          (""),                                   (""),               (""))                    \
+  DO(SS_KILL_WHOLE_LINE,   (SS_LCTL("a") SS_LCTL("k")),          (""),                                   (""),               (""))                    \
+  DO(EM_LASTARG,           (" " SS_LCTL("c") "."),               (""),                                   (""),               (""))                    \
+  DO(SS_GUI_CLICK,         (GUI_CLICK()),                        (GUI_CLICK_AND_TAB()),                  (""),               (GUI_CLICK_AND_TAB()))   \
+  DO(SS_DICT,              (T(X_F24) T(X_F24)),                  (SS_DOWN(X_F24) T(X_SPC) SS_UP(X_F24)), (""),               (""))                    \
+  DO(SS_PIN1,              (AE_PIN1),                            (AE_PIN2),                              (ROUTER_PWD),       (AE_FPWD))               \
+  DO(SS_0X,                (")x"),                               (""),                                   (""),               (")X"))                  \
+  DO(SS_ARROW,             ("->"),                               (" => "),                               ("490" LL()),       (""))                    \
+  DO(SS_SMILEY,            (" ;0"),                              (" :/"),                                (" >_>"),           (""))                    \
+  DO(SS_SMILEY2,           (" :0"),                              (" :P"),                                (" :D"),            (""))                    \
+  DO(SS_DIR,               ("~/"),                               ("../"),                                ("./"),             (""))                    \
+  DO(SS_CD,                ("cd "),                              ("cd ~/"),                              ("cd .."),          ("cd -"))                \
+  DO(SS_LBRACK,            ("9"),                                ("{"),                                  ("["),              ("<"))                   \
+  DO(SS_RBRACK,            ("0"),                                ("}"),                                  ("]"),              (">"))                   \
+  DO(SS_AND_AND,           (" 77 "),                             (" || "),                               (" @>7! "),         (" >/dev/null "))        \
+//                         NO MODS                               CTRL                                    ALT                 SHIFT
+#endif
 
 #define enum_item(kc, str, ...)                                                                kc,
 #define define_tagged_progmem_string(tag, kc, str, ...)                                        static const char tag##_str_##kc[] PROGMEM = str;
