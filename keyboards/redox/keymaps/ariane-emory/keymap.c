b07d8d4d9c63ad19f690d1228caccee00840cce5
Ariane Emory <ariane.emory@protonmail.com>
2024-03-20 19:41:58 -0400

    prettify Rust symbols

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -93,7 +93,7 @@ void keyboard_post_init_user(void) {
 
 #define TAP(ss_kc)             SS_TAP(ss_kc)
 #define S_CR(_)                TAP(X_ENT)
-#define S_DD()                 SS_DELAY(250)
+#define S_DD()                 SS_DELAY(150)
 #define S_ESC()                TAP(X_ESC)
 #define S_LL(_)                TAP(X_LEFT)
 #define S_RR(_)                TAP(X_RIGHT)
