797251fbfed7a301006b0759abfbfc3f4753789e
Ariane Emory <ariane.emory@protonmail.com>
2024-12-04 17:50:37 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -555,10 +555,6 @@ static const struct { uint16_t matched; uint16_t tapped; } tap_cases[] PROGMEM =
   { LHRM_1(VD_RIGHT_ALT),        VD_RIGHT       },
   { LHRM_2(VD_LEFT_ALT),         VD_LEFT        },
 
-  /* { LHRM_4(LCA(KC_B)),           LCA(KC_B)      }, */
-  /* { LHRM_3(LCA(KC_F)),           LCA(KC_F)      },  */
-
-  // dummy keys to C(A) / C(E):
   { LHRM_4(LCA(KC_B)),           LCA(KC_B)      },
   { LHRM_3(LCA(KC_F)),           LCA(KC_F)      }, 
 
