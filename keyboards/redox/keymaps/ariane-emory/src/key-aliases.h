1227c3e4f055dc11d5cd0029345f42d365dcdca8
Ariane Emory <ariane.emory@protonmail.com>
2025-05-17 06:28:53 -0400

    ARROWS_OPEN

--- keyboards/redox/keymaps/ariane-emory/src/key-aliases.h
+++ keyboards/redox/keymaps/ariane-emory/src/key-aliases.h
@@ -120,6 +120,7 @@
 #define SPOTLIGHT      LGUI(KC_SPC)
 #define TG_MOUSE       TG(LN_MOUSE)
 #define OTHER_WIN2     LGUI(KC_GRV)
+#define ARROWS_OPEN    LT(LN_ARROWS, LGUI(KC_O))
 //==============================================================================
 
 
