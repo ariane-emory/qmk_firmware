83f66fc38f550bfaa7c68dbfb0b396163e4dc63c
Ariane Emory <ariane.emory@protonmail.com>
2025-09-24 22:40:54 -0400

    let's come up with...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -993,6 +993,9 @@ void leader_end_user(void) {
   if (leader_sequence_two_keys(KC_B, KC_B)) {
     my_boot_handler(0, NULL);
   }
+  else if (leader_sequence_one_key(KC_P)) {
+    SEND_STRING_WITHOUT_MODS_P(PSTR("Let's break the problem down into small steps and come up with a detailed, step-by-step plan to implement "));
+  }
   else if (leader_sequence_one_key(KC_A)) {
     tap_code16(C(KC_A));
   }
