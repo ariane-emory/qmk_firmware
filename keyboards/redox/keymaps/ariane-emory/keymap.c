2a40538dc76b0a9dc1577e6771a6872389b9c4fe
Ariane Emory <ariane.emory@protonmail.com>
2025-09-27 03:30:27 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -791,7 +791,7 @@ void matrix_scan_user(void) {
 #endif // TOGGLED_LAYER_TIMEOUT
   
 #if defined(RGBLIGHT_ENABLE) && defined(MY_RGB_LAYERS)
-  if (!set_rgb_led_fader_target_if_recording_macro(&rgb_led_fader))
+  // if (!set_rgb_led_fader_target_if_recording_macro(&rgb_led_fader))
     set_rgb_led_fader_target_by_layer(&rgb_led_fader);
 
 #  ifdef SLOW_RGBS
