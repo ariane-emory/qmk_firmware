4e0f7b7ef8be9238c39c5346a7955af470ecf9e7
Ariane Emory <ariane.emory@protonmail.com>
2025-09-26 20:40:11 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -989,12 +989,16 @@ uint16_t keycode_config(uint16_t keycode) {
 static const char git_str[] PROGMEM = "git ";
 
 #ifdef LEADER_ENABLE
+void ss_do_not_edit(void) {
+  SEND_STRING_WITHOUT_MODS_P(PSTR("Do not edit any code yet, for now I "))
+}
+
 void leader_end_user(void) {
   if (leader_sequence_two_keys(KC_B, KC_B)) {
     my_boot_handler(0, NULL);
   }
-  else if (leader_sequence_two_keys(KC_A, KC_A)) { /* ask */ SEND_STRING_WITHOUT_MODS_P(PSTR("Do not edit any code yet, for now I am just asking you questions about the current code. "));}
-  else if (leader_sequence_two_keys(KC_A, KC_E)) { /* analyze and explain */ SEND_STRING_WITHOUT_MODS_P(PSTR("Do not edit any code yet, for now I am just need you to analyze and explain this problem: "));} 
+  else if (leader_sequence_two_keys(KC_A, KC_A)) { /* ask */ ss_do_not_edit(); SEND_STRING_WITHOUT_MODS_P(PSTR("for now I am just asking you questions about the current code. "));}
+  else if (leader_sequence_two_keys(KC_A, KC_E)) { /* analyze and explain */ ss_do_not_edit(); SEND_STRING_WITHOUT_MODS_P(PSTR("for now I am just need you to analyze and explain this problem: "));} 
   else if (leader_sequence_two_keys(KC_A, KC_F)) { /* analyze and fix */ SEND_STRING_WITHOUT_MODS_P(PSTR("Please analyze this problem and fix it: "));}
   else if (leader_sequence_two_keys(KC_A, KC_S)) { /* analyze smells */ SEND_STRING_WITHOUT_MODS_P(PSTR("Right now, your job is not to edit any code, your job is instead to study the codebase and analyze it, looking for things that could be refactored to improve its maintainability and extensibility, or other 'code smells' that we could eliminate. Once you have come up with a detailed list of things that could be improved, submit it to me for approval by writing it to ./refactor-plan.md. "));}
   else if (leader_sequence_two_keys(KC_D, KC_N)) { /* do it now, no mistakes */ SEND_STRING_WITHOUT_MODS_P(PSTR("Do it now, do it correctly, and do not make any mistakes. "));}
