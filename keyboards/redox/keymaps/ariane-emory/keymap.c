e0b8036a798194546e1032f32d8bb7ade4d7189d
Ariane Emory <ariane.emory@protonmail.com>
2025-09-28 23:15:52 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1056,18 +1056,12 @@ void ss_write_the_plan_(void) {
   SEND_STRING_WITHOUT_MODS_P(PSTR("Write the plan "));
 }
 
-void ss_refactoring(void) {
-  SEND_STRING_WITHOUT_MODS_P(PSTR("refactoring"));
-}
-
 void sses_plan_refactor_(void) {
   ss_lets_think_it_through_thoroughly();
   _ss_and_break_the_();
-  ss_refactoring();
+  SEND_STRING_WITHOUT_MODS_P(PSTR("refactoring"));
   _sses_down_into_small_steps_to_come_up_with_a_detailed_step_by_step_plan_for_how_to_implement_();
-  SEND_STRING_WITHOUT_MODS_P(PSTR("this "));
-  ss_refactoring();
-  SEND_STRING_WITHOUT_MODS_P(PSTR("\b. "));
+  SEND_STRING_WITHOUT_MODS_P(PSTR("this refactoring. "));
 }
 
 void sses_write_the_plan_in_the_planmd_file_(void) {
