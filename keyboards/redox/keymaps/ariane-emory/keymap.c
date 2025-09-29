2dd3ab430dd6413eada7132c07aa1d83160db775
Ariane Emory <ariane.emory@protonmail.com>
2025-09-28 23:05:53 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -999,7 +999,11 @@ void ss_do_not_edit_any_code_yet_bang_(void) {
   SEND_STRING_WITHOUT_MODS_P(PSTR("Do not edit any code yet! "));
 }
 
-void ss_submit_the_plan_for_approval_(void) {
+void ss_detailed_step_by_step_plan_(void) {
+   SEND_STRING_WITHOUT_MODS_P(PSTR("detailed, step-by-step plan"));
+}
+
+void ss_plan_and_submit_the_plan_for_approval_(void) {
   SEND_STRING_WITHOUT_MODS_P(PSTR("Once you have come up with a detailed plan, submit it to me for approval "));
 }
 
@@ -1090,7 +1094,7 @@ void leader_end_user(void) {
     ss_do_not_edit_any_code_yet_bang_();
     SEND_STRING_WITHOUT_MODS_P(PSTR("Your job is to analyze the code for opportunities to refactor to improve its maintainability, or other 'code smells' we could eliminate. "));
     sses_plan_refactor_();
-    ss_submit_the_plan_for_approval_();
+    ss_plan_and_submit_the_plan_for_approval_();
     ss_afterwards_dot_();
     sses_write_the_plan_in_the_planmd_file_();
   }
@@ -1144,7 +1148,7 @@ void leader_end_user(void) {
     SEND_STRING_WITHOUT_MODS_P(PSTR("For now I am just asking you questions about the code. "));
   }
   else if (leader_sequence_two_keys(KC_S, KC_A)) { /* submit for approval */
-    ss_submit_the_plan_for_approval_(); 
+    ss_plan_and_submit_the_plan_for_approval_(); 
     SEND_STRING_WITHOUT_MODS_P(PSTR("before you start editing the code. "));
   }
   // other:
