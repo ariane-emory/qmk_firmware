b6c1305a4580e7247e3067efd98fb687ecd149fd
Ariane Emory <ariane.emory@protonmail.com>
2025-09-28 23:10:04 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1013,8 +1013,10 @@ void ss_lets_think_it_through_thoroughly(void) {
   SEND_STRING_WITHOUT_MODS_P(PSTR("Let's think it through thoroughly"));
 }
 
-void _ss_down_into_small_steps_to_come_up_with_a_detailed_step_by_step_plan_for_how_to_implement_(void) {
-  SEND_STRING_WITHOUT_MODS_P(PSTR(" down into small steps to come up with a detailed, step-by-step plan for how to implement "));
+void _sses_down_into_small_steps_to_come_up_with_a_detailed_step_by_step_plan_for_how_to_implement_(void) {
+  SEND_STRING_WITHOUT_MODS_P(PSTR(" down into small steps to come up with a "));
+  ss_detailed_step_by_step_plan_();
+  SEND_STRING_WITHOUT_MODS_P(PSTR("for how to implement "));
 }
 
 void ss_the_code_must_build_correctly_(void) {
@@ -1058,7 +1060,7 @@ void sses_plan_refactor_(void) {
   ss_lets_think_it_through_thoroughly();
   _ss_and_break_the_();
   SEND_STRING_WITHOUT_MODS_P(PSTR("refactoring"));
-  _ss_down_into_small_steps_to_come_up_with_a_detailed_step_by_step_plan_for_how_to_implement_();
+  _sses_down_into_small_steps_to_come_up_with_a_detailed_step_by_step_plan_for_how_to_implement_();
   SEND_STRING_WITHOUT_MODS_P(PSTR("this refactoring. "));
 }
 
@@ -1132,14 +1134,14 @@ void leader_end_user(void) {
     ss_lets_think_it_through_thoroughly();
     _ss_and_break_the_();
     SEND_STRING_WITHOUT_MODS_P(PSTR("feature"));
-    _ss_down_into_small_steps_to_come_up_with_a_detailed_step_by_step_plan_for_how_to_implement_();
+    _sses_down_into_small_steps_to_come_up_with_a_detailed_step_by_step_plan_for_how_to_implement_();
     SEND_STRING_WITHOUT_MODS_P(PSTR("the feature. "));
   }
   else if (leader_sequence_two_keys(KC_P, KC_P)) { /* plan problem */
     ss_lets_think_it_through_thoroughly();
     _ss_and_break_the_();
     SEND_STRING_WITHOUT_MODS_P(PSTR("problem"));
-    _ss_down_into_small_steps_to_come_up_with_a_detailed_step_by_step_plan_for_how_to_implement_();
+    _sses_down_into_small_steps_to_come_up_with_a_detailed_step_by_step_plan_for_how_to_implement_();
     SEND_STRING_WITHOUT_MODS_P(PSTR("a solution. "));
   }
   else if (leader_sequence_two_keys(KC_P, KC_R)) { /* plan refactor */
