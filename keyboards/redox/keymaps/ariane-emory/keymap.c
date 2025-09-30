712688a41a5d5e307b4089e796e44ad02703614d
Ariane Emory <ariane.emory@protonmail.com>
2025-09-30 15:02:32 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -130,12 +130,12 @@ void keyboard_post_init_user(void) {
   DO(SS_FULLSCR,           (SS_DOWN(X_F24) TAP(X_F) SS_UP(X_F24)))                                                                               \
   DO(EM_SWAP,              S_EM_SWAP()                           )                                                                               \
   DO(SS_KILL_WHOLE_LINE,   (SS_LCTL("a") SS_LCTL("k"))           )                                                                               \
-  DO(EM_SHELL,             (SS_LCTL("x") "t")           )                                                                               \
+  DO(EM_SHELL,             (SS_LCTL("x") "t")           )                                                                                        \
   DO(EM_ALL_BUFF,          (SS_LCTL("x") SS_LCTL("b"))           )                                                                               \
   DO(EM_DIRED,             (SS_LCTL("x") SS_LCTL("j"))           )                                                                               \
-  DO(EM_KILL_BUFF,         (SS_LCTL("x") "k")           )                                                                               \
+  DO(EM_KILL_BUFF,         (SS_LCTL("x") "k")           )                                                                                        \
   DO(EM_LWRAP,             (SS_LCTL("x") SS_LCTL("l"))           )                                                                               \
-  DO(EM_PRV_BUFF,          (SS_LCTL("x") "p"                     )                                                                               \
+  DO(EM_PRV_BUFF,          (SS_LCTL("x") "p")                    )                                                                               \
   DO(EM_SPLIT_V,           (SS_LCTL("x") "@")                    )                                                                               \
   DO(EM_SPLIT_H,           (SS_LCTL("x") "#")                    )                                                                               \
   DO(EM_MAC_BGN,           (SS_LCTL("x") "9")                    )                                                                               \
@@ -151,10 +151,10 @@ void keyboard_post_init_user(void) {
   DO(SS_FULLSCR,           (SS_DOWN(X_F24) TAP(X_F) SS_UP(X_F24)))                                                                               \
   DO(EM_SWAP,              S_EM_SWAP()                           )                                                                               \
   DO(SS_KILL_WHOLE_LINE,   (SS_LCTL("a") SS_LCTL("k"))           )                                                                               \
-  DO(EM_SHELL,             (SS_LCTL("x") "t")           )                                                                               \
+  DO(EM_SHELL,             (SS_LCTL("x") "t")           )                                                                                        \
   DO(EM_ALL_BUFF,          (SS_LCTL("x") SS_LCTL("b"))           )                                                                               \
   DO(EM_DIRED,             (SS_LCTL("x") SS_LCTL("j"))           )                                                                               \
-  DO(EM_KILL_BUFF,         (SS_LCTL("x") "k")           )                                                                               \
+  DO(EM_KILL_BUFF,         (SS_LCTL("x") "k")           )                                                                                        \
   DO(EM_LWRAP,             (SS_LCTL("x") SS_LCTL("l"))           )                                                                               \
   DO(EM_PRV_BUFF,          (SS_LCTL("x") "p")                    )                                                                               \
   DO(EM_SPLIT_V,           (SS_LCTL("x") "2")                    )                                                                               \
@@ -181,35 +181,35 @@ void keyboard_post_init_user(void) {
      (S_PP_EVAL_SEXP),        /* CTRL */                                                                                                         \
      (" " SS_LCTL("c") "."),  /* ALT */                                                                                                          \
      ("c -" S_CR()))          /* SHIFT */                                                                                                        \
-  DO(SS_GUI_CLICK,         (S_GUI_CLICK()),         (S_GUI_CLICK_AND_TAB()),                  (""),               (S_GUI_CLICK_AND_TAB()))   \
-  DO(SS_PIN1,              (AE_PIN1),               (AE_PIN2),                                (ROUTER_PWD),       (AE_FPWD))                 \
-  DO(SS_ARROW,             ("->"),                  (" => "),                                 ("4()" S_LL()),     (""))                      \
-  DO(SS_SMILEY,            (" ;0"),                 (" :/"),                                  (" >_>"),           (""))                      \
-  DO(SS_SMILEY2,           (" :0"),                 (" :P"),                                  (" :D"),            (""))                      \
-  DO(SS_DIR,               ("~/"),                  ("../"),                                  ("./"),             (""))                      \
-  DO(SS_CD,                ("cd "),                 ("cd ~/"),                                ("cd .."),          ("cd -"))                  \
-  DO(SS_LBRACK,            ("9"),                   ("{"),                                    ("["),              ("<"))                     \
-  DO(SS_RBRACK,            ("0"),                   ("}"),                                    ("]"),              (">"))                     \
-  DO(SS_AND_AND,           (" 77 "),                (" || "),                                 (" @>7! "),         (" >/dev/null "))          \
+  DO(SS_GUI_CLICK,         (S_GUI_CLICK()),         (S_GUI_CLICK_AND_TAB()),                  (""),               (S_GUI_CLICK_AND_TAB()))       \
+  DO(SS_PIN1,              (AE_PIN1),               (AE_PIN2),                                (ROUTER_PWD),       (AE_FPWD))                     \
+  DO(SS_ARROW,             ("->"),                  (" => "),                                 ("4()" S_LL()),     (""))                          \
+  DO(SS_SMILEY,            (" ;0"),                 (" :/"),                                  (" >_>"),           (""))                          \
+  DO(SS_SMILEY2,           (" :0"),                 (" :P"),                                  (" :D"),            (""))                          \
+  DO(SS_DIR,               ("~/"),                  ("../"),                                  ("./"),             (""))                          \
+  DO(SS_CD,                ("cd "),                 ("cd ~/"),                                ("cd .."),          ("cd -"))                      \
+  DO(SS_LBRACK,            ("9"),                   ("{"),                                    ("["),              ("<"))                         \
+  DO(SS_RBRACK,            ("0"),                   ("}"),                                    ("]"),              (">"))                         \
+  DO(SS_AND_AND,           (" 77 "),                (" || "),                                 (" @>7! "),         (" >/dev/null "))              \
 //                         NO MODS                  CTRL                                      ALT                 SHIFT
 #else
-#  define FOR_EACH_MODDABLE_SEND_STRING_KEYCODE(DO)                                                                                          \
-  DO(SS_CD,                ("cd "),                 ("cd ~/"),                                ("cd .."),          ("cd -"))                  \
-  DO(SS_SMILEY2,           (" :)"),                 (" :P"),                                  (" :D"),            (""))                      \
-  DO(SS_AND_AND,           (" && "),                (" || "),                                 (" 2>&1 "),         (" >/dev/null "))          \
-  DO(SS_SMILEY,            (" ;)"),                 (" :/"),                                  (" >_>"),           (""))                      \
-  DO(SS_LBRACK,            ("("),                   ("{"),                                    ("["),              ("<"))                     \
-  DO(SS_RBRACK,            (")"),                   ("}"),                                    ("]"),              (">"))                     \
-  DO(EM_LASTARG,                                                                                                                             \
-     (S_EVAL_SEXP),           /* NO MODS */                                                                                                  \
-     (S_PP_EVAL_SEXP),        /* CTRL */                                                                                                     \
-     (" " SS_LCTL("c") "."),  /* ALT */                                                                                                      \
-     ("c -" S_CR()))          /* SHIFT */                                                                                                    \
-  DO(SS_DICT,              (SS_DOWN(X_F24) TAP(X_S) SS_UP(X_F24)),  (TAP(X_F24) TAP(X_F24)),  (""),               (""))                      \
+#  define FOR_EACH_MODDABLE_SEND_STRING_KEYCODE(DO)                                                                                              \
+  DO(SS_CD,                ("cd "),                 ("cd ~/"),                                ("cd .."),          ("cd -"))                      \
+  DO(SS_SMILEY2,           (" :)"),                 (" :P"),                                  (" :D"),            (""))                          \
+  DO(SS_AND_AND,           (" && "),                (" || "),                                 (" 2>&1 "),         (" >/dev/null "))              \
+  DO(SS_SMILEY,            (" ;)"),                 (" :/"),                                  (" >_>"),           (""))                          \
+  DO(SS_LBRACK,            ("("),                   ("{"),                                    ("["),              ("<"))                         \
+  DO(SS_RBRACK,            (")"),                   ("}"),                                    ("]"),              (">"))                         \
+  DO(EM_LASTARG,                                                                                                                                 \
+     (S_EVAL_SEXP),           /* NO MODS */                                                                                                      \
+     (S_PP_EVAL_SEXP),        /* CTRL */                                                                                                         \
+     (" " SS_LCTL("c") "."),  /* ALT */                                                                                                          \
+     ("c -" S_CR()))          /* SHIFT */                                                                                                        \
+  DO(SS_DICT,              (SS_DOWN(X_F24) TAP(X_S) SS_UP(X_F24)),  (TAP(X_F24) TAP(X_F24)),  (""),               (""))                          \
   DO(SS_DIR,               ("~/"),                  ("../"),                                  ("./"),             ("` + `" S_LL() S_CR() S_TB() S_RR())) \
-  DO(SS_ARROW,             (" => "),                ("${}" S_LL()),                           ("``" S_LL()),      ("->"))                    \
-  DO(SS_GUI_CLICK,         (S_GUI_CLICK()),         (S_GUI_CLICK_AND_TAB()),                  (""),               (S_GUI_CLICK_AND_TAB()))   \
-  DO(SS_PIN1,              (AE_PIN1),               (AE_PIN2),                                (ROUTER_PWD),       (AE_FPWD))                 \
+  DO(SS_ARROW,             (" => "),                ("${}" S_LL()),                           ("``" S_LL()),      ("->"))                        \
+  DO(SS_GUI_CLICK,         (S_GUI_CLICK()),         (S_GUI_CLICK_AND_TAB()),                  (""),               (S_GUI_CLICK_AND_TAB()))       \
+  DO(SS_PIN1,              (AE_PIN1),               (AE_PIN2),                                (ROUTER_PWD),       (AE_FPWD))                     \
 //                         NO MODS                  CTRL                                      ALT                 SHIFT
 #endif
 
