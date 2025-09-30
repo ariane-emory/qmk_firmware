833a7ba7b1db7e46df6b8453f0c952d268fa42ce
Ariane Emory <ariane.emory@protonmail.com>
2025-09-30 15:22:05 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1106,30 +1106,14 @@ void ss_write_the_plan_in_the_planmd_file_(void) {
 // leader_end_user
 // =============================================================================
 void leader_end_user(void) {
-  if (leader_sequence_two_keys(KC_B, KC_B)) {
-    my_boot_handler(0, NULL);
-  }
-  else if (leader_sequence_one_key(KC_A)) {
-    tap_code16(C(KC_A));
-  }
-  else if (leader_sequence_one_key(KC_E)) {
-    tap_code16(C(KC_E));
-  }
-  else if (leader_sequence_two_keys(KC_D, KC_D)) {
-    tap_code16(KC_F5);
-  }
-  else if (leader_sequence_one_key(KC_D)) {
-    tap_code16(S(KC_DEL));
-  }
-  else if (leader_sequence_two_keys(KC_D, KC_D)) {
-    tap_code16(C(KC_F5));
-  }
-  else if (leader_sequence_two_keys(KC_A, KC_A)) {
-    tap_code16(C(KC_A));
-  }
-  else if (leader_sequence_two_keys(KC_E, KC_E)) {
-    tap_code16(C(KC_E));
-  }
+  if (leader_sequence_two_keys(KC_B, KC_B)) {my_boot_handler(0, NULL);}
+  else if (leader_sequence_one_key(KC_A)) {tap_code16(C(KC_A));}
+  else if (leader_sequence_one_key(KC_E)) {tap_code16(C(KC_E));}
+  else if (leader_sequence_two_keys(KC_D, KC_D)) {tap_code16(KC_F5);}
+  else if (leader_sequence_one_key(KC_D)) {tap_code16(S(KC_DEL));}
+  else if (leader_sequence_two_keys(KC_D, KC_D)) {tap_code16(C(KC_F5));}
+  else if (leader_sequence_two_keys(KC_A, KC_A)) {tap_code16(C(KC_A));}
+  else if (leader_sequence_two_keys(KC_E, KC_E)) {tap_code16(C(KC_E));}
   else if (leader_sequence_three_keys(KC_R, KC_R, KC_R)) {
     SEND_STRING_WITHOUT_MODS_P(PSTR(S_END() S_CLR_LINE()));
     tap_code16(A(KC_P));
