c8ca71f2b0559e6c6604a3e8e514a7fe200f022c
Ariane Emory <ariane.emory@protonmail.com>
2025-09-30 14:57:12 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1206,7 +1206,7 @@ void leader_end_user(void) {
   else if (leader_sequence_two_keys(KC_G, KC_H)) {SEND_STRING_WITHOUT_MODS_P(PSTR("git checkout "));}
   else if (leader_sequence_two_keys(KC_G, KC_M)) {SEND_STRING_WITHOUT_MODS_P(PSTR("git merge "));}
   else if (leader_sequence_two_keys(KC_G, KC_P)) {SEND_STRING_WITHOUT_MODS_P(PSTR("git push " S_CR()));}
-  else if (leader_sequence_three_keys(KC_G, KC_P, KC_U)) {SEND_STRING_WITHOUT_MODS_P(PSTR("git pull " S_CR()));}
+  else if (leader_sequence_two_keys(KC_G, KC_U)) {SEND_STRING_WITHOUT_MODS_P(PSTR("git pull " S_CR()));}
   else if (leader_sequence_three_keys(KC_G, KC_R, KC_H)) {SEND_STRING_WITHOUT_MODS_P(PSTR("git reset --hard " S_CR()));}
 }
 #endif // LEADER_ENABLE
