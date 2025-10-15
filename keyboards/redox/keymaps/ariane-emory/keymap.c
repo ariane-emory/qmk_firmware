82969545a179ff3ec5871e795a84b41280a0dd76
Ariane Emory <ariane.emory@protonmail.com>
2025-10-15 18:54:06 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -137,7 +137,7 @@ void keyboard_post_init_user(void) {
   DO(EM_DIRED,             (SS_LCTL("x") SS_LCTL("j"))           )                                                                               \
   DO(EM_KILL_BUFF,         (SS_LCTL("x") SS_LCTL("k"))           )                                                                               \
   DO(EM_LWRAP,             (SS_LCTL("x") SS_LCTL("l"))           )                                                                               \
-  DO(EM_PRV_BUFF,          (SS_LCTL("x") SS_LCTL("p"))           )                                                                               \
+  DO(EM_PRV_BUFF,          (SS_LCTL("x") "p")                    )                                                                               \
   DO(EM_SPLIT_V,           (SS_LCTL("x") "@")                    )                                                                               \
   DO(EM_SPLIT_H,           (SS_LCTL("x") "#")                    )                                                                               \
   DO(EM_MAC_BGN,           (SS_LCTL("x") "9")                    )                                                                               \
@@ -158,7 +158,7 @@ void keyboard_post_init_user(void) {
   DO(EM_DIRED,             (SS_LCTL("x") SS_LCTL("j"))           )                                                                               \
   DO(EM_KILL_BUFF,         (SS_LCTL("x") SS_LCTL("k"))           )                                                                               \
   DO(EM_LWRAP,             (SS_LCTL("x") SS_LCTL("l"))           )                                                                               \
-  DO(EM_PRV_BUFF,          (SS_LCTL("x") SS_LCTL("p"))           )                                                                               \
+  DO(EM_PRV_BUFF,          (SS_LCTL("x") "p")                    )                                                                               \
   DO(EM_SPLIT_V,           (SS_LCTL("x") "2")                    )                                                                               \
   DO(EM_SPLIT_H,           (SS_LCTL("x") "3")                    )                                                                               \
   DO(EM_MAC_BGN,           (SS_LCTL("x") "(")                    )                                                                               \
@@ -835,49 +835,20 @@ static const uint16_t achordion_canary_bilat_keys[] PROGMEM = {
 
 static const keycode_pair_t achordion_exceptions[] PROGMEM = {
   // Left GUI
-  // { QH_S,    QT_TAB          }, // app switcher, one of these is redundant but I haven't figured out which one yet.
-  // { QH_S,    KC_TAB          }, // app switcher, one of these is redundant but I haven't figured out which one yet.
-  // { QH_S,    QB_Z            }, // undo
   { QH_S,    QT_R            }, // refresh
   { QH_S,    QT_T            }, // new tab
-  // { QH_S,    QB_C            }, // copy
-  // { QH_S,    QB_V            }, // paste
-  
-  // Left Alt
-  // { QH_D,    KC_TAB          }, // alt+tab
-  // { QH_D,    QT_W            }, // close
-  // { QH_D,    QT_R            }, // refresh
-  // { QH_D,    QH_F            }, // forwards word
-  // { QH_D,    QB_B            }, // backwards word
-  // { QH_D,    QT_T            }, // new tab
-  { QH_F,    KC_ENT          }, // C-<return>
+  { QH_S,   KC_ENT },
+
+  // Left alt
   { QH_D,    KC_ENT          }, // M-<return>
 
   // Left Control
   { QH_F,    QH_A            }, // beginning of line
-  // { NH_J,    NH_QUOT         }, // beginning of line
+  { QH_F,    KC_ENT          }, // C-<return>
   { QH_F,    QT_E            }, // end of line
-  // { NH_J,    NH_K            }, // end of line
-  
-  // { QH_F,    QH_D            }, // delete forwards char
-  // { QH_F,    QT_T            }, // new tab
-  // { QH_F,    QT_W            }, // close
-  // { QH_F,    QH_S            }, // i-search
-
-  // Left shift
-  // { QH_G,    KC_SPC },
-  // Right  shift
-  // { QH_H,    KC_SPC },
-  // Left shift
-  // { QH_A,    KC_SPC },
-  // Right  shift
-  // { QH_QUOT, KC_SPC },
   
   // Right Control
-  // { QH_J,    QT_U            }, // emacs prefix arg
-
   { QH_J,    QH_K            }, // kill line
-  { NH_F,    NT_T            }, // kill line
 
   { QH_J,    QH_L            }, // recenter / address bar
   { QH_J,    QT_Y            }, // yank
@@ -888,13 +859,8 @@ static const keycode_pair_t achordion_exceptions[] PROGMEM = {
   // Right Alt
   { QH_K,    QH_L            }, // address bar?
   { QH_K,    QT_P            }, // prev command
-  /* { QH_K,    KC_BSLS         }, // ??? */
   { QH_K,    QB_N            }, // next command
 
-  { QH_S,   KC_ENT },
-  // Right GUI
-  /* { QH_L,    QH_K            }, // ??? */
-  /* { QH_L,    KC_BSLS         }, // ??? */
 };
 
 bool achordion_chord(
@@ -1159,7 +1125,8 @@ void leader_end_user(void) {
   else if (leader_sequence_one_key (KC_S))       {SEND_PSTR(S_END() S_CLR_LINE() "shove" S_CR());}
   /* prompt fragments: */
   else if (leader_sequence_two_keys(KC_C, KC_C)) { /* continue */ SEND_PSTR("Continue. ");}
-  else if (leader_sequence_two_keys(KC_C, KC_P)) { /* continue */ SEND_PSTR("Complete the ENTIRE plan! ");}
+  else if (leader_sequence_two_keys(KC_C, KC_E)) { /* correct all errors */ SEND_PSTR("Correct ALL the errors! ");}
+  else if (leader_sequence_two_keys(KC_C, KC_P)) { /* complete plan */ SEND_PSTR("Complete the ENTIRE plan! ");}
   else if (leader_sequence_two_keys(KC_D, KC_N)) { /* do it now, no mistakes */ SEND_PSTR("Do it now, do it correctly, and make no mistakes. ");}
   else if (leader_sequence_two_keys(KC_G, KC_W)) { /* great work */ SEND_PSTR("Great work! ");}
   else if (leader_sequence_two_keys(KC_K, KC_K)) { /* keep going */ SEND_PSTR("Keep going. ");}
