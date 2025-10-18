7617d65fe2300566d80976bb9ec9ae6373813762
Ariane Emory <ariane.emory@protonmail.com>
2025-10-18 16:24:41 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -959,6 +959,7 @@ uint16_t keycode_config(uint16_t keycode) {
 // ==============================================================================
 
 #define S_END()      TAP(X_END)
+#define S_BOL()      SS_LCTL(TAP(X_A))
 #define S_CLR()      SS_LGUI("a") TAP(X_BSPC) S_CR()
 #define S_CLR_LINE() S_END() SS_LCTL("e") SS_LCTL(TAP(X_SPC)) SS_LCTL("a") TAP(X_BSPC)
 
@@ -1135,8 +1136,9 @@ void leader_end_user(void) {
   else if (leader_sequence_one_key   (KC_Q))       {SEND_PSTR(/*S_CLR()*/ "cdkm; qmkc" S_CR());}
   else if (leader_sequence_two_keys  (KC_Q, KC_W)) {SEND_PSTR(S_CLR() "cdkm; qmkupd" S_CR());}
   else if (leader_sequence_one_key   (KC_R))       {SEND_PSTR(S_REPEAT_SHELL_CMD(_));}
-  else if (leader_sequence_one_key   (KC_S))       {SEND_PSTR(S_END() S_CLR_LINE() "shove" S_CR());}
-  /* prompt fragments: */
+  //else if (leader_sequence_one_key   (KC_S))       {SEND_PSTR(S_END() S_BOL() SS_LCTL(TAP(X_K)) "shove" S_CR());}
+  else if (leader_sequence_one_key   (KC_S))       {SEND_PSTR(S_END() SS_LCTL(TAP(X_A)) SS_LCTL(TAP(X_K)) "shove" S_CR());}
+/* prompt fragments: */
   else if (leader_sequence_one_key   (KC_C))       { /* continue */ SEND_PSTR("Continue. ");}
   else if (leader_sequence_two_keys  (KC_C, KC_E)) { /* correct all errors */ SEND_PSTR("Correct ALL of the errors! ");}
   else if (leader_sequence_two_keys  (KC_C, KC_P)) { /* complete plan */ SEND_PSTR("You MUST complete the ENTIRE plan! ");}
