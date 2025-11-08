59adff63fbc0c84463a0a9ee54288ad39191cd7a
Ariane Emory <ariane.emory@protonmail.com>
2025-11-07 22:57:17 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/config.h
+++ keyboards/redox/keymaps/ariane-emory/config.h
@@ -76,7 +76,8 @@
 #define MY_RGB_FLIP_LAYER_ON                 0x88, 0x00, 0xC0 // violet-ish
 #define MY_RGB_LOWER_LAYER_ON                0x00, 0x74, 0x1C // blue-ish green
 #define MY_RGB_UPPER_LAYER_ON                0xFF, 0x00, 0x70 // magenta-ish
-#define MY_RGB_ADJUST_LAYER_ON               0x00, 0x14, 0xC0 // blue 
+#define MY_RGB_ADJUST_LAYER_ON               0x00, 0x14, 0xC0 // blue
+#define MY_RGB_TOGGLED_ARROWS_LAYER_ON       0x00, 0x34, 0xF0 // blue 
 #define MY_RGB_TOGGLED_LAYER_ON              0x10, 0x90, 0x00 // green
 #define MY_RGB_BOOT                          0xFF, 0x28, 0x00 // orange-ish
 #define MY_RGB_WORKMAK                       0xFF, 0x20, 0x10 // pink-ish
