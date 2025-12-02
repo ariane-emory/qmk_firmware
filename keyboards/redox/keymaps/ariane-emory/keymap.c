2048427b0bd9175d194385c3771b8db9ae81ad50
Ariane Emory <ariane.emory@protonmail.com>
2025-12-02 05:08:33 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1131,6 +1131,7 @@ void matrix_scan_user(void) {
       /* prompt fragments: */
     else if (leader_sequence_one_key   (KC_C))       { /* continue */ SEND_PSTR("Continue. ");}
     else if (leader_sequence_two_keys  (KC_C, KC_E)) { /* correct all errors */ SEND_PSTR("Correct ALL of the errors! ");}
+    else if (leader_sequence_two_keys  (KC_C, KC_O)) { /* carry on */ SEND_PSTR("Carry on. ");}
     else if (leader_sequence_two_keys  (KC_C, KC_P)) { /* complete plan */ SEND_PSTR("You MUST complete the ENTIRE plan! ");}
     else if (leader_sequence_two_keys  (KC_D, KC_P)) { /* diagnose problem */ ss_diagnose_();}
       /* else if (leader_sequence_two_keys  (KC_D, KC_N)) { /\* do it now, no mistakes *\/ SEND_PSTR("Do it now, do it correctly, and make no mistakes. ");} */
