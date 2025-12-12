987aac87b0987494fd57043a9190ad554a18dbd5
Ariane Emory <ariane.emory@protonmail.com>
2025-12-12 05:02:38 -0500

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -1148,8 +1148,9 @@ void matrix_scan_user(void) {
     else if (leader_sequence_two_keys  (KC_P, KC_S)) { /* proceed systematically */ SEND_PSTR("Proceed systematically ");}
     else if (leader_sequence_two_keys  (KC_R, KC_T)) { /* try again */ SEND_PSTR("Rethink it and then try again! Ultrathink!");}
     else if (leader_sequence_two_keys  (KC_S, KC_F)) { /* so far so good */ SEND_PSTR("So far, so good. ");}
-    else if (leader_sequence_two_keys  (KC_S, KC_G)) { /* sounds good */ SEND_PSTR("Sounds good good to me. ");}
+    else if (leader_sequence_two_keys  (KC_S, KC_G)) { /* sounds good */ SEND_PSTR("Sounds good to me. ");}
       /* else if (leader_sequence_two_keys  (KC_T, KC_C)) { /\* testing comms *\/ SEND_PSTR("Testing communications, can you hear me? ");} */
+    else if (leader_sequence_two_keys  (KC_T, KC_A)) { /* try again */ SEND_PSTR("Try again. ");}
     else if (leader_sequence_two_keys  (KC_T, KC_H)) { /* think hard */ SEND_PSTR("Think hard! ");}
     else if (leader_sequence_two_keys  (KC_T, KC_P)) { /* problem persists */ SEND_PSTR("The problem persists. "); }
       /* else if (leader_sequence_two_keys  (KC_T, KC_Y)) { /\* thank you *\/ SEND_PSTR("Thank you. ");} */
