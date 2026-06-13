2fc053758c05ef46c497f7efa9bfb10cc1ba9c2f
Ariane Emory <ariane.emory@protonmail.com>
2026-06-13 08:59:02 -0400

    ...

--- keyboards/redox/keymaps/ariane-emory/keymap.c
+++ keyboards/redox/keymaps/ariane-emory/keymap.c
@@ -186,10 +186,9 @@ DO(SS_PIN1,              (AE_PIN1),               (AE_PIN2),               (ROUT
 #define define_ctrled_progmem_string(kc, nomods_str, ctrled_str, ...)                          define_tagged_progmem_string(ctrled, kc, ctrled_str, __VA_ARGS__)
 #define define_alted_progmem_string(kc, nomods_str, ctrled_str, alted_str, ...)                define_tagged_progmem_string(alted, kc, alted_str, __VA_ARGS__)
 #define define_shifted_progmem_string(kc, nomods_str, ctrled_str, alted_str, shifted_str, ...) define_tagged_progmem_string(shifted, kc, shifted_str, __VA_ARGS__)
-
 #define define_basic_progmem_string(kc, nomods_str)                                            define_tagged_progmem_string(nomods, kc, nomods_str)
-FOR_EACH_BASIC_SEND_STRING_KEYCODE(define_basic_progmem_string);
 
+FOR_EACH_BASIC_SEND_STRING_KEYCODE(define_basic_progmem_string);
 FOR_EACH_MODDABLE_SEND_STRING_KEYCODE(define_nomods_progmem_string);
 FOR_EACH_MODDABLE_SEND_STRING_KEYCODE(define_alted_progmem_string);
 FOR_EACH_MODDABLE_SEND_STRING_KEYCODE(define_ctrled_progmem_string);
